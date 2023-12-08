a2cedf73c0f5a0f35820001be9259ec5d8a848da
Meier Lukas <meierschlumpf@gmail.com>
2023-12-08 22:35:15 +0100

    Initial commit

---
+++ packages/db/index.ts
@@ -0,0 +1,20 @@
+import { Client } from "@planetscale/database";
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+
+import * as auth from "./schema/auth";
+import * as post from "./schema/post";
+
+export const schema = { ...auth, ...post };
+
+export { mySqlTable as tableCreator } from "./schema/_table";
+
+export * from "drizzle-orm";
+
+export const db = drizzle(
+  new Client({
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  }).connection(),
+  { schema },
+);
