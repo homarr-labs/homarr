f142838da46254494e1121ea463378065d78b04f
Meier Lukas <meierschlumpf@gmail.com>
2024-02-15 20:14:31 +0100

    fix: deepsource issue JS-0415

--- apps/nextjs/src/app/[locale]/layout.tsx
+++ apps/nextjs/src/app/[locale]/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "@homarr/ui/styles.css";
 import "@homarr/notifications/styles.css";
 import "@homarr/spotlight/styles.css";
+import "@homarr/ui/styles.css";
 
 import { headers } from "next/headers";
 
@@ -18,6 +18,7 @@ import { JotaiProvider } from "./_client-providers/jotai";
 import { ModalsProvider } from "./_client-providers/modals";
 import { NextInternationalProvider } from "./_client-providers/next-international";
 import { TRPCReactProvider } from "./_client-providers/trpc";
+import { composeWrappers } from "./compose";
 
 const fontSans = Inter({
   subsets: ["latin"],
@@ -42,27 +43,32 @@ export default function Layout(props: {
 }) {
   const colorScheme = "dark";
 
+  const StackedProvider = composeWrappers([
+    (innerProps) => <JotaiProvider {...innerProps} />,
+    (innerProps) => <TRPCReactProvider {...innerProps} headers={headers()} />,
+    (innerProps) => (
+      <NextInternationalProvider {...innerProps} locale={props.params.locale} />
+    ),
+    (innerProps) => (
+      <MantineProvider
+        {...innerProps}
+        defaultColorScheme={colorScheme}
+        {...uiConfiguration}
+      />
+    ),
+    (innerProps) => <ModalsProvider {...innerProps} />,
+  ]);
+
   return (
     <html lang="en">
       <head>
         <ColorSchemeScript defaultColorScheme={colorScheme} />
       </head>
       <body className={["font-sans", fontSans.variable].join(" ")}>
-        <JotaiProvider>
-          <TRPCReactProvider headers={headers()}>
-            <NextInternationalProvider locale={props.params.locale}>
-              <MantineProvider
-                defaultColorScheme={colorScheme}
-                {...uiConfiguration}
-              >
-                <ModalsProvider>
-                  <Notifications />
-                  {props.children}
-                </ModalsProvider>
-              </MantineProvider>
-            </NextInternationalProvider>
-          </TRPCReactProvider>
-        </JotaiProvider>
+        <StackedProvider>
+          <Notifications />
+          {props.children}
+        </StackedProvider>
       </body>
     </html>
   );
