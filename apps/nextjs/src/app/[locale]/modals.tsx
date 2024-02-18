e3e38e7f4496989a761657d9e79c317e26738c46
Manuel <30572287+manuel-rw@users.noreply.github.com>
2024-02-18 15:44:49 +0100

    feat: add create board modal (#131)

--- apps/nextjs/src/app/[locale]/modals.tsx
+++ apps/nextjs/src/app/[locale]/modals.tsx
@@ -6,9 +6,11 @@ import { WidgetEditModal } from "@homarr/widgets";
 
 import { ItemSelectModal } from "~/components/board/items/item-select-modal";
 import { CategoryEditModal } from "~/components/board/sections/category/category-edit-modal";
+import { AddBoardModal } from "~/components/manage/boards/add-board-modal";
 
 export const [ModalsManager, modalEvents] = createModalManager({
   categoryEditModal: CategoryEditModal,
   widgetEditModal: WidgetEditModal,
   itemSelectModal: ItemSelectModal,
+  addBoardModal: AddBoardModal,
 });
