3ad83094dab80b157bdb0e160e89d41d917da5fc
Meier Lukas <meierschlumpf@gmail.com>
2024-03-09 19:52:28 +0100

    fix: ci issues

--- packages/api/src/router/board.ts
+++ packages/api/src/router/board.ts
@@ -19,6 +19,7 @@ import {
 } from "@homarr/validation";
 
 import { zodUnionFromArray } from "../../../validation/src/enums";
+import type { WidgetComponentProps } from "../../../widgets/src/definition";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 const filterAddedItems = <TInput extends { id: string }>(
