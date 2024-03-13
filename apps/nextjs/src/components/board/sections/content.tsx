208959b665a98ffa5868802a0c644ba711659a8d
Meier Lukas <meierschlumpf@gmail.com>
2024-03-13 08:26:05 +0100

    fix: remove unnecessary type convertion

--- apps/nextjs/src/components/board/sections/content.tsx
+++ apps/nextjs/src/components/board/sections/content.tsx
@@ -58,7 +58,7 @@ export const SectionContent = ({ items, refs }: Props) => {
             ref={refs.items.current[item.id] as RefObject<HTMLDivElement>}
           >
             <Card
-              ref={ref as RefObject<HTMLDivElement>}
+              ref={ref}
               className={cx(classes.itemCard, "grid-stack-item-content")}
               withBorder
               styles={{
