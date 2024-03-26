56d3d552a9c5249c47e6a10a9c7c15678a2f3296
Manuel <30572287+manuel-rw@users.noreply.github.com>
2024-03-26 19:25:28 +0100

    fix: dispose terminal correctly

--- apps/nextjs/src/app/[locale]/manage/tools/logs/terminal.tsx
+++ apps/nextjs/src/app/[locale]/manage/tools/logs/terminal.tsx
@@ -1,27 +1,33 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { Terminal } from "@xterm/xterm";
 import { CanvasAddon } from "@xterm/addon-canvas";
+import { Terminal } from "@xterm/xterm";
 
 import { clientApi } from "@homarr/api/client";
 
 export default function TerminalComponent() {
   const ref = useRef<HTMLDivElement>(null);
-  const terminalRef = useRef<Terminal>(new Terminal({
-    cursorBlink: false,
-    disableStdin: true,
-    convertEol: true,
-  }));
+  const renderedBeforeRef = useRef<boolean>(false);
+
+  const terminalRef = useRef<Terminal>(
+    new Terminal({
+      cursorBlink: false,
+      disableStdin: true,
+      convertEol: true,
+    }),
+  );
 
   clientApi.log.subscribe.useSubscription(undefined, {
     onData(data) {
-      terminalRef.current.writeln(`${data.timestamp} ${data.level} ${data.message}`);
+      terminalRef.current.writeln(
+        `${data.timestamp} ${data.level} ${data.message}`,
+      );
       terminalRef.current.refresh(0, terminalRef.current.rows - 1);
     },
     onError(err) {
       alert(err);
-    }
+    },
   });
 
   useEffect(() => {
@@ -38,14 +44,19 @@ export default function TerminalComponent() {
     terminalRef.current.open(ref.current);
     terminalRef.current.loadAddon(canvasAddon);
 
-    console.log('created on ref', ref.current);
-
     return () => {
-      // terminalRef.current.dispose();
-      console.log('disposed');
-      // fitAddon.dispose();
-      // canvasAddon.dispose();
+      if (renderedBeforeRef.current) {
+        terminalRef.current.dispose();
+        canvasAddon.dispose();
+      }
+      renderedBeforeRef.current = true;
     };
   }, []);
-  return <div ref={ref} id="terminal" style={{ height: 400, backgroundColor: 'red' }}></div>;
+  return (
+    <div
+      ref={ref}
+      id="terminal"
+      style={{ height: 400, backgroundColor: "red" }}
+    ></div>
+  );
 }
