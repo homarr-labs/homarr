a6819fed6e3d97e0d5747908dc3e8051fffbf592
Meier Lukas <meierschlumpf@gmail.com>
2024-05-10 19:26:25 +0200

    fix: issue with driver seems to be fixed

--- packages/db/configs/sqlite.config.ts
+++ packages/db/configs/sqlite.config.ts
@@ -5,7 +5,6 @@ dotenv.config({ path: "../../.env" });
 
 export default {
   dialect: "sqlite",
-  driver: "better-sqlite" as never, // TODO: Remove this line when the issue is fixed
   schema: "./schema",
   dbCredentials: { url: process.env.DB_URL! },
   out: "./migrations/sqlite",
