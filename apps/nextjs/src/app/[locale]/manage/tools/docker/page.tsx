e50dbecb3720a9089101fcc40a2c9146c7582006
ajnart <ajnart@pm.me>
2024-05-19 11:33:47 +0200

    feat: Refactor DockerPage to use DockerTable component

--- apps/nextjs/src/app/[locale]/manage/tools/docker/page.tsx
+++ apps/nextjs/src/app/[locale]/manage/tools/docker/page.tsx
@@ -2,20 +2,14 @@ import { Box } from "@mantine/core";
 
 import { api } from "@homarr/api/server";
 
+import { DockerTable } from "./DockerTable";
+
 export default async function DockerPage() {
   const { containers, timestamp } = await api.docker.getContainers();
-  const timeAgo = new Intl.RelativeTimeFormat("en", {
-    style: "narrow",
-  }).format((new Date().getTime() - new Date(timestamp).getTime()) / 1000, "seconds");
   return (
     <Box style={{ borderRadius: 6 }} p="md" bg="black">
       <h2>Welcome to docker !</h2>
-      <p>Last time this was fetched was: {timeAgo} ago</p>
-      <ul>
-        {containers.map((container) => (
-          <li key={container.id}>{container.name}</li>
-        ))}
-      </ul>
+      <DockerTable containers={containers} timestamp={timestamp} />
     </Box>
   );
 }
