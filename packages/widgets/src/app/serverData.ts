5e2176194d1fc5087eb9685423319d792cc999cc
Meier Lukas <meierschlumpf@gmail.com>
2024-06-03 20:58:32 +0200

    test: add some unit tests

--- packages/widgets/src/app/serverData.ts
+++ packages/widgets/src/app/serverData.ts
@@ -6,11 +6,15 @@ import { api } from "@homarr/api/server";
 import type { WidgetProps } from "../definition";
 
 export default async function getServerDataAsync({ options }: WidgetProps<"app">) {
+  if (!options.appId) {
+    return { app: null, pingResult: null };
+  }
+
   try {
     const app = await api.app.byId({ id: options.appId });
     let pingResult: RouterOutputs["widget"]["app"]["ping"] | null = null;
 
-    if (app.href && options.openInNewTab) {
+    if (app.href && options.pingEnabled) {
       pingResult = await api.widget.app.ping({ url: app.href });
     }
 
