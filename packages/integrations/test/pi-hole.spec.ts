421fea9de5273f8b0a4d800a2cd500f01142daba
Meier Lukas <meierschlumpf@gmail.com>
2024-06-10 06:33:35 +0200

    refactor: improve structure of pi hole test

--- packages/integrations/test/pi-hole.spec.ts
+++ packages/integrations/test/pi-hole.spec.ts
@@ -1,33 +1,16 @@
-import { GenericContainer, Wait } from "testcontainers";
+import { GenericContainer, StartedTestContainer, Wait } from "testcontainers";
 import { describe, expect, test } from "vitest";
 
 import { PiHoleIntegration } from "../src";
 
+const DEFAULT_PASSWORD = "12341234";
+const DEFAULT_API_KEY = "3b1434980677dcf53fa8c4a611db3b1f0f88478790097515c0abb539102778b9"; // Some hash generated from password
+
 describe("Pi-hole integration", () => {
   test("getSummaryAsync should return summary from pi-hole", async () => {
     // Arrange
-    const password = "12341234";
-    const apiKey = "3b1434980677dcf53fa8c4a611db3b1f0f88478790097515c0abb539102778b9";
-
-    const piholeContainer = await new GenericContainer("pihole/pihole:latest")
-      .withEnvironment({
-        WEBPASSWORD: password,
-      })
-      .withExposedPorts(80)
-      .withWaitStrategy(Wait.forLogMessage(/Pi-hole Enabled/))
-      .start();
-
-    const piHoleIntegration = new PiHoleIntegration({
-      id: "1",
-      decryptedSecrets: [
-        {
-          kind: "apiKey",
-          value: apiKey,
-        },
-      ],
-      name: "Pi hole",
-      url: `http://${piholeContainer.getHost()}:${piholeContainer.getMappedPort(80)}`,
-    });
+    const piholeContainer = await createPiHoleContainer(DEFAULT_PASSWORD).start();
+    const piHoleIntegration = createPiHoleIntegration(piholeContainer, DEFAULT_API_KEY);
 
     // Act
     const result = await piHoleIntegration.getSummaryAsync();
@@ -42,3 +25,26 @@ describe("Pi-hole integration", () => {
     await piholeContainer.stop();
   }, 20_000); // Timeout of 20 seconds
 });
+
+const createPiHoleContainer = (password: string) => {
+  return new GenericContainer("pihole/pihole:latest")
+    .withEnvironment({
+      WEBPASSWORD: password,
+    })
+    .withExposedPorts(80)
+    .withWaitStrategy(Wait.forLogMessage("Pi-hole Enabled"));
+};
+
+const createPiHoleIntegration = (container: StartedTestContainer, apiKey: string) => {
+  return new PiHoleIntegration({
+    id: "1",
+    decryptedSecrets: [
+      {
+        kind: "apiKey",
+        value: apiKey,
+      },
+    ],
+    name: "Pi hole",
+    url: `http://${container.getHost()}:${container.getMappedPort(80)}`,
+  });
+};
