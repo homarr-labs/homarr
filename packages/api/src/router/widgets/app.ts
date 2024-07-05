be6f7127c83bc00dd79413213139367abe744252
Meier Lukas <meierschlumpf@gmail.com>
2024-07-05 22:00:35 +0200

    fix: memory leak caused by many unclosed redis subscriptions

--- packages/api/src/router/widgets/app.ts
+++ packages/api/src/router/widgets/app.ts
@@ -27,19 +27,15 @@ export const appRouter = createTRPCRouter({
       const pingResult = await sendPingRequestAsync(input.url);
 
       return observable<{ url: string; statusCode: number } | { url: string; error: string }>((emit) => {
-        let isConnectionClosed = false;
-
         emit.next({ url: input.url, ...pingResult });
-        pingChannel.subscribe((message) => {
-          if (isConnectionClosed) return;
-
+        const unsubscribe = pingChannel.subscribe((message) => {
           // Only emit if same url
           if (message.url !== input.url) return;
           emit.next(message);
         });
 
         return () => {
-          isConnectionClosed = true;
+          unsubscribe();
           void pingUrlChannel.removeAsync(input.url);
         };
       });
---
+++ packages/redis/src/lib/subscription-manager.ts
@@ -0,0 +1,83 @@
+import { randomUUID } from "crypto";
+
+import type { MaybePromise } from "@homarr/common/types";
+import { logger } from "@homarr/log";
+
+import { createRedisConnection } from "./connection";
+
+type SubscriptionCallback = (message: string) => MaybePromise<void>;
+
+export class SubscriptionManager {
+  private static subscriptions = new Map<string, Map<string, SubscriptionCallback>>();
+  private static redis = createRedisConnection();
+  private static listenerActive = false;
+
+  /**
+   * Subscribes to a channel.
+   * @param channelName name of the channel
+   * @param callback callback function to be called when a message is received
+   * @returns a function to unsubscribe from the channel
+   */
+  public static subscribe(channelName: string, callback: SubscriptionCallback) {
+    logger.debug(`Adding redis channel callback channel='${channelName}'`);
+
+    // We only want to activate the listener once
+    if (!this.listenerActive) {
+      this.activateListener();
+      this.listenerActive = true;
+    }
+
+    const channelSubscriptions = this.subscriptions.get(channelName) ?? new Map<string, SubscriptionCallback>();
+    const id = randomUUID();
+
+    // If there are no subscriptions to the channel, subscribe to it
+    if (channelSubscriptions.size === 0) {
+      logger.debug(`Subscribing to redis channel channel='${channelName}'`);
+      void this.redis.subscribe(channelName);
+    }
+
+    logger.debug(`Adding redis channel callback channel='${channelName}' id='${id}'`);
+    channelSubscriptions.set(id, callback);
+
+    this.subscriptions.set(channelName, channelSubscriptions);
+
+    // Return a function to unsubscribe
+    return () => {
+      logger.debug(`Removing redis channel callback channel='${channelName}' id='${id}'`);
+
+      const channelSubscriptions = this.subscriptions.get(channelName);
+      if (!channelSubscriptions) return;
+
+      channelSubscriptions.delete(id);
+
+      // If there are no subscriptions to the channel, unsubscribe from it
+      if (channelSubscriptions.size === 0) {
+        logger.debug(`Unsubscribing from redis channel channel='${channelName}'`);
+        void this.redis.unsubscribe(channelName);
+        this.subscriptions.delete(channelName);
+      }
+    };
+  }
+
+  /**
+   * Activates the listener for the redis client.
+   */
+  private static activateListener() {
+    logger.debug("Activating listener");
+    this.redis.on("message", (channel, message) => {
+      const channelSubscriptions = this.subscriptions.get(channel);
+      if (!channelSubscriptions) {
+        logger.warn(`Received message on unknown channel channel='${channel}'`);
+        return;
+      }
+
+      for (const [id, callback] of channelSubscriptions.entries()) {
+        // Don't log messages from the logging channel as it would create an infinite loop
+        if (channel !== "pubSub:logging") {
+          logger.debug(`Calling subscription callback channel='${channel}' id='${id}'`);
+        }
+        void callback(message);
+      }
+    });
+  }
+}
