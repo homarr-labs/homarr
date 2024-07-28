08f1374fa0a458afd7975eb619bcf4adfc48830c
Meier Lukas <meierschlumpf@gmail.com>
2024-07-28 09:51:32 +0200

    fix: fetch timeout for external requests to small

---
+++ packages/common/src/fetch-with-timeout.ts
@@ -0,0 +1,17 @@
+/**
+ * Same as fetch, but with a timeout of 10 seconds.
+ * https://stackoverflow.com/questions/46946380/fetch-api-request-timeout
+ * @param param0 fetch arguments
+ * @returns fetch response
+ */
+export const fetchWithTimeout = (...[url, requestInit]: Parameters<typeof fetch>) => {
+  const controller = new AbortController();
+
+  // 10 seconds timeout:
+  const timeoutId = setTimeout(() => controller.abort(), 10000);
+
+  return fetch(url, { signal: controller.signal, ...requestInit }).then((response) => {
+    clearTimeout(timeoutId);
+    return response;
+  });
+};
