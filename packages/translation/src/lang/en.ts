b53a2b6b78f73f86ecff24cbf40b91e0f4ca55b0
Meier Lukas <meierschlumpf@gmail.com>
2024-08-04 21:19:31 +0200

    feat: add refresh button to docker containers table (#913)

--- packages/translation/src/lang/en.ts
+++ packages/translation/src/lang/en.ts
@@ -1656,6 +1656,19 @@ export default {
           },
         },
       },
+      refresh: {
+        label: "Refresh",
+        notification: {
+          success: {
+            title: "Containers refreshed",
+            message: "You are now viewing the most recent data",
+          },
+          error: {
+            title: "Containers not refreshed",
+            message: "Something went wrong while refreshing the containers",
+          },
+        },
+      },
     },
   },
   permission: {
