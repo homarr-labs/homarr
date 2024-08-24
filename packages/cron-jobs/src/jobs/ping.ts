b81f5a2ee349f1b8205a86d72d00c4a2e3470d1f
Meier Lukas <meierschlumpf@gmail.com>
2024-08-24 16:56:40 +0200

    fix: improve ping performance (#1020)

--- packages/cron-jobs/src/jobs/ping.ts
+++ packages/cron-jobs/src/jobs/ping.ts
@@ -15,18 +15,20 @@ export const pingJob = createCronJob("ping", EVERY_MINUTE, {
 }).withCallback(async () => {
   const urls = await pingUrlChannel.getAllAsync();
 
-  for (const url of new Set(urls)) {
-    const pingResult = await sendPingRequestAsync(url);
-
-    if ("statusCode" in pingResult) {
-      logger.debug(`executed ping for url ${url} with status code ${pingResult.statusCode}`);
-    } else {
-      logger.error(`Executing ping for url ${url} failed with error: ${pingResult.error}`);
-    }
-
-    await pingChannel.publishAsync({
-      url,
-      ...pingResult,
-    });
-  }
+  await Promise.allSettled([...new Set(urls)].map(pingAsync));
 });
+
+const pingAsync = async (url: string) => {
+  const pingResult = await sendPingRequestAsync(url);
+
+  if ("statusCode" in pingResult) {
+    logger.debug(`executed ping for url ${url} with status code ${pingResult.statusCode}`);
+  } else {
+    logger.error(`Executing ping for url ${url} failed with error: ${pingResult.error}`);
+  }
+
+  await pingChannel.publishAsync({
+    url,
+    ...pingResult,
+  });
+};
