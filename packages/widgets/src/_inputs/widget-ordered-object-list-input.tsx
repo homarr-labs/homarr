ea42577c3f6c366ef8989cdfdf89437dd629ff35
Manuel <manuel.ruwe@protonmail.com>
2024-09-06 22:11:20 +0200

    wip

--- packages/widgets/src/_inputs/widget-ordered-object-list-input.tsx
+++ packages/widgets/src/_inputs/widget-ordered-object-list-input.tsx
@@ -1,27 +1,33 @@
-import { Button, Card, Stack, Text } from "@mantine/core";
-import type { CommonWidgetInputProps } from "./common";
-import { useFormContext } from "./form";
+import {Button, Card, Stack, Text} from "@mantine/core";
+import type {CommonWidgetInputProps} from "./common";
+import {useFormContext} from "./form";
 import React from "react";
 
 export const WidgetOrderedObjectListInput = ({
-  property,
-  options,
-}: CommonWidgetInputProps<"orderedObjectList">) => {
+                                               property,
+                                               options,
+                                             }: CommonWidgetInputProps<"orderedObjectList">) => {
   const form = useFormContext();
 
   const values = form.values.options[property] as Record<string, unknown>[];
 
+  const add = () => {
+    form.setFieldValue('options', [...form.values.options, {
+      name: ''
+    }]);
+  }
+
   return (
     <Card>
       {values.map((value, index) => {
-        return <options.itemComponent key={index} item={value} />;
+        return <options.itemComponent key={index} item={value}/>;
       })}
       {values.length === 0 && (
         <Stack align={"center"} mb={"xl"}>
           <Text c={"dimmed"}>Not configured any items</Text>
         </Stack>
       )}
-      <Button>Add item</Button>
+      <Button onClick={add}>Add item</Button>
     </Card>
   );
 };
