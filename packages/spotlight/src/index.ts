a52db86839cbc6506502f587fc0ceadb04348678
Meier Lukas <meierschlumpf@gmail.com>
2024-09-18 18:44:28 +0200

    chore: remove unused code

--- packages/spotlight/src/index.ts
+++ packages/spotlight/src/index.ts
@@ -3,7 +3,6 @@
 import { spotlightActions } from "./spotlight-store";
 
 export { Spotlight } from "./components/spotlight";
-export { useRegisterSpotlightActions } from "./data-store";
 export { openSpotlight };
 
 const openSpotlight = spotlightActions.open;
