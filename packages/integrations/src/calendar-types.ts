5a2debb00090d29d7817d0a72b483c938c896357
hillaliy <hillaliy@me.com>
2024-10-06 20:47:48 +0300

    fix: type check

--- packages/integrations/src/calendar-types.ts
+++ packages/integrations/src/calendar-types.ts
@@ -2,7 +2,7 @@ export interface CalendarEvent {
   name: string;
   subName: string;
   date: Date;
-  releaseType?: "Cinemas" | "Physical" | "Digital";
+  releaseType?: "Cinemas" | "Physical" | "Digital" | undefined;
   description?: string;
   thumbnail?: string;
   mediaInformation?: {
