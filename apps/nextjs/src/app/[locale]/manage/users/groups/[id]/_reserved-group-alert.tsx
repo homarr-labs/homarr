e12c7b05e5e5b88ce92fe2154e659e27ab7ea8b3
Meier Lukas <meierschlumpf@gmail.com>
2024-10-18 17:49:18 +0200

    feat: improve user interface for everyone group

---
+++ apps/nextjs/src/app/[locale]/manage/users/groups/[id]/_reserved-group-alert.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { Alert, Anchor } from "@mantine/core";
+import { IconExclamationCircle } from "@tabler/icons-react";
+
+import { useI18n } from "@homarr/translation/client";
+
+export const ReservedGroupAlert = () => {
+  const t = useI18n();
+
+  return (
+    <Alert variant="light" color="yellow" icon={<IconExclamationCircle size="1rem" stroke={1.5} />}>
+      {t("group.reservedNotice.message", {
+        checkoutDocs: (
+          <Anchor
+            size="sm"
+            component={Link}
+            href="https://deploy-preview-113--homarr-docs.netlify.app/docs/management/users/#special-groups"
+            target="_blank"
+          >
+            {t("common.action.checkoutDocs")}
+          </Anchor>
+        ),
+      })}
+    </Alert>
+  );
+};
