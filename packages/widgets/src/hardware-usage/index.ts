b7428a20647720c7ba44068d841f1a3a29dc404b
Manuel <30572287+manuel-rw@users.noreply.github.com>
2024-10-20 15:44:08 +0200

    fix: refactor from server data to suspense query

--- packages/widgets/src/hardware-usage/index.ts
+++ packages/widgets/src/hardware-usage/index.ts
@@ -1,14 +1,11 @@
 import { IconVideo } from "@tabler/icons-react";
 
-import { z } from "@homarr/validation";
-
 import { createWidgetDefinition } from "../definition";
 import { optionsBuilder } from "../options";
 
-export const { componentLoader, definition, serverDataLoader } = createWidgetDefinition("hardwareUsage", {
+export const { componentLoader, definition } = createWidgetDefinition("hardwareUsage", {
   icon: IconVideo,
   supportedIntegrations: ["getDashDot"],
   options: optionsBuilder.from((factory) => ({})),
 })
-  .withServerData(() => import("./serverData"))
   .withDynamicImport(() => import("./component"));
