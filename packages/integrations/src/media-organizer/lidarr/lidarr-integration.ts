a35e7a1d08487562192ebb4dbf2adc2fbb677d2a
Manuel <30572287+manuel-rw@users.noreply.github.com>
2024-11-06 20:12:08 +0100

    fix: formatting

--- packages/integrations/src/media-organizer/lidarr/lidarr-integration.ts
+++ packages/integrations/src/media-organizer/lidarr/lidarr-integration.ts
@@ -10,7 +10,7 @@ export class LidarrIntegration extends Integration {
    * Types at the start of the list are better than those at the end.
    * We do this to attempt to find the best quality image for the show.
    */
-  private readonly priorities: z.infer<typeof radarrCalendarEventSchema>["images"][number]["coverType"][] = [
+  private readonly priorities: z.infer<typeof lidarrCalendarEventSchema>["images"][number]["coverType"][] = [
     "cover", // Official, perfect aspect ratio
     "poster", // Official, perfect aspect ratio
     "banner", // Official, bad aspect ratio
@@ -29,7 +29,41 @@ export class LidarrIntegration extends Integration {
     });
   }
 
-  private getLinksForRadarrCalendarEvent = (event: z.infer<typeof radarrCalendarEventSchema>) => {
+  /**
+   * Gets the events in the Radarr calendar between two dates.
+   * @param start The start date
+   * @param end The end date
+   * @param includeUnmonitored When true results will include unmonitored items of the Tadarr library.
+   */
+  async getCalendarEventsAsync(start: Date, end: Date, includeUnmonitored = true): Promise<CalendarEvent[]> {
+    const url = new URL(this.integration.url);
+    url.pathname = "/api/v1/calendar";
+    url.searchParams.append("start", start.toISOString());
+    url.searchParams.append("end", end.toISOString());
+    url.searchParams.append("unmonitored", includeUnmonitored ? "true" : "false");
+    const response = await fetch(url, {
+      headers: {
+        "X-Api-Key": super.getSecretValue("apiKey"),
+      },
+    });
+    const radarrCalendarEvents = await z.array(lidarrCalendarEventSchema).parseAsync(await response.json());
+
+    return radarrCalendarEvents.map((radarrCalendarEvent): CalendarEvent => {
+      return {
+        name: radarrCalendarEvent.title,
+        subName: radarrCalendarEvent.artist.artistName,
+        description: radarrCalendarEvent.overview,
+        thumbnail: this.chooseBestImageAsURL(radarrCalendarEvent),
+        date: radarrCalendarEvent.releaseDate,
+        mediaInformation: {
+          type: "audio",
+        },
+        links: this.getLinksForRadarrCalendarEvent(radarrCalendarEvent),
+      };
+    });
+  }
+
+  private getLinksForRadarrCalendarEvent = (event: z.infer<typeof lidarrCalendarEventSchema>) => {
     const links: CalendarEvent["links"] = [];
 
     if (event.artist.links.some((link) => link.name === "vgmdb")) {
@@ -84,8 +118,8 @@ export class LidarrIntegration extends Integration {
   };
 
   private chooseBestImage = (
-    event: z.infer<typeof radarrCalendarEventSchema>,
-  ): z.infer<typeof radarrCalendarEventSchema>["images"][number] | undefined => {
+    event: z.infer<typeof lidarrCalendarEventSchema>,
+  ): z.infer<typeof lidarrCalendarEventSchema>["images"][number] | undefined => {
     const flatImages = [...event.images];
 
     const sortedImages = flatImages.sort(
@@ -95,60 +129,26 @@ export class LidarrIntegration extends Integration {
     return sortedImages[0];
   };
 
-  private chooseBestImageAsURL = (event: z.infer<typeof radarrCalendarEventSchema>): string | undefined => {
+  private chooseBestImageAsURL = (event: z.infer<typeof lidarrCalendarEventSchema>): string | undefined => {
     const bestImage = this.chooseBestImage(event);
     if (!bestImage) {
       return undefined;
     }
     return bestImage.remoteUrl;
   };
-
-  /**
-   * Gets the events in the Radarr calendar between two dates.
-   * @param start The start date
-   * @param end The end date
-   * @param includeUnmonitored When true results will include unmonitored items of the Tadarr library.
-   */
-  async getCalendarEventsAsync(start: Date, end: Date, includeUnmonitored = true): Promise<CalendarEvent[]> {
-    const url = new URL(this.integration.url);
-    url.pathname = "/api/v1/calendar";
-    url.searchParams.append("start", start.toISOString());
-    url.searchParams.append("end", end.toISOString());
-    url.searchParams.append("unmonitored", includeUnmonitored ? "true" : "false");
-    const response = await fetch(url, {
-      headers: {
-        "X-Api-Key": super.getSecretValue("apiKey"),
-      },
-    });
-    const radarrCalendarEvents = await z.array(radarrCalendarEventSchema).parseAsync(await response.json());
-
-    return radarrCalendarEvents.map((radarrCalendarEvent): CalendarEvent => {
-      return {
-        name: radarrCalendarEvent.title,
-        subName: radarrCalendarEvent.artist.artistName,
-        description: radarrCalendarEvent.overview,
-        thumbnail: this.chooseBestImageAsURL(radarrCalendarEvent),
-        date: radarrCalendarEvent.releaseDate,
-        mediaInformation: {
-          type: "audio",
-        },
-        links: this.getLinksForRadarrCalendarEvent(radarrCalendarEvent),
-      };
-    });
-  }
 }
 
-const radarrCalendarEventImageSchema = z.array(
+const lidarrCalendarEventImageSchema = z.array(
   z.object({
     coverType: z.enum(["screenshot", "poster", "banner", "fanart", "clearlogo", "cover"]),
     remoteUrl: z.string().url(),
   }),
 );
 
-const radarrCalendarEventSchema = z.object({
+const lidarrCalendarEventSchema = z.object({
   title: z.string(),
   overview: z.string().optional(),
-  images: radarrCalendarEventImageSchema,
+  images: lidarrCalendarEventImageSchema,
   artist: z.object({ links: z.array(z.object({ url: z.string().url(), name: z.string() })), artistName: z.string() }),
   releaseDate: z.string().transform((value) => new Date(value)),
 });
