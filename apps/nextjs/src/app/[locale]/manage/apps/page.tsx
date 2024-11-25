7e349bb04bba665112b41cdbd40f8e76be2e8381
Meier Lukas <meierschlumpf@gmail.com>
2024-11-25 17:31:14 +0100

    refactor: improve design of no integrations found (#1543)
    * refactor: improve design of no integrations found
    
    * fix: deepsource issue

--- apps/nextjs/src/app/[locale]/manage/apps/page.tsx
+++ apps/nextjs/src/app/[locale]/manage/apps/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ActionIcon, ActionIconGroup, Anchor, Avatar, Card, Group, Stack, Text, Title } from "@mantine/core";
-import { IconApps, IconPencil } from "@tabler/icons-react";
+import { IconBox, IconPencil } from "@tabler/icons-react";
 
 import type { RouterOutputs } from "@homarr/api";
 import { api } from "@homarr/api/server";
@@ -12,6 +12,7 @@ import { getI18n, getScopedI18n } from "@homarr/translation/server";
 import { ManageContainer } from "~/components/manage/manage-container";
 import { MobileAffixButton } from "~/components/manage/mobile-affix-button";
 import { DynamicBreadcrumb } from "~/components/navigation/dynamic-breadcrumb";
+import { NoResults } from "~/components/no-results";
 import { AppDeleteButton } from "./_app-delete-button";
 
 export default async function AppsPage() {
@@ -113,16 +114,14 @@ const AppNoResults = async () => {
   const session = await auth();
 
   return (
-    <Card withBorder bg="transparent">
-      <Stack align="center" gap="sm">
-        <IconApps size="2rem" />
-        <Text fw={500} size="lg">
-          {t("app.page.list.noResults.title")}
-        </Text>
-        {session?.user.permissions.includes("app-create") && (
-          <Anchor href="/manage/apps/new">{t("app.page.list.noResults.action")}</Anchor>
-        )}
-      </Stack>
-    </Card>
+    <NoResults
+      icon={IconBox}
+      title={t("app.page.list.noResults.title")}
+      action={{
+        label: t("app.page.list.noResults.action"),
+        href: "/manage/apps/new",
+        hidden: !session?.user.permissions.includes("app-create"),
+      }}
+    />
   );
 };
