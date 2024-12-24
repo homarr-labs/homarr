8df398c3c706369f6990719603a357c161606f81
Meier Lukas <meierschlumpf@gmail.com>
2024-12-24 14:16:24 +0100

    feat(apps): remove url variables (#1771)

--- apps/nextjs/src/app/[locale]/manage/apps/page.tsx
+++ apps/nextjs/src/app/[locale]/manage/apps/page.tsx
@@ -6,7 +6,6 @@ import { IconBox, IconPencil } from "@tabler/icons-react";
 import type { RouterOutputs } from "@homarr/api";
 import { api } from "@homarr/api/server";
 import { auth } from "@homarr/auth/next";
-import { parseAppHrefWithVariablesServer } from "@homarr/common/server";
 import { getI18n, getScopedI18n } from "@homarr/translation/server";
 
 import { ManageContainer } from "~/components/manage/manage-container";
@@ -82,8 +81,8 @@ const AppCard = async ({ app }: AppCardProps) => {
               </Text>
             )}
             {app.href && (
-              <Anchor href={parseAppHrefWithVariablesServer(app.href)} lineClamp={1} size="sm" w="min-content">
-                {parseAppHrefWithVariablesServer(app.href)}
+              <Anchor href={app.href} lineClamp={1} size="sm" w="min-content">
+                {app.href}
               </Anchor>
             )}
           </Stack>
--- packages/common/src/server.ts
+++ packages/common/src/server.ts
@@ -1,3 +1,2 @@
-export * from "./app-url/server";
 export * from "./security";
 export * from "./encryption";
