f2b26a303b11a77ec6c9d78f29cfb8633026f01c
oussama Dahmaz <dahmaz@MacBook-Pro-de-odahmaz.local>
2025-01-12 17:06:40 +0100

    feat(kubernetes): add kubernetes tool

---
+++ apps/nextjs/src/app/[locale]/manage/tools/kubernetes/cluster-dashboard/header-card/header-card.tsx
@@ -0,0 +1,55 @@
+import { Card, Flex, Text, ThemeIcon } from "@mantine/core";
+import { IconBrandGit, IconCloudShare, IconGeometry } from "@tabler/icons-react";
+
+import { isLocaleRTL } from "@homarr/translation";
+import { useCurrentLocale, useI18n } from "@homarr/translation/client";
+
+import classes from "./header-card.module.css";
+
+type HeaderTypes = "providers" | "version" | "architecture";
+
+interface HeaderCardProps {
+  headerType: HeaderTypes;
+  value: string;
+}
+
+export function HeaderCard(props: HeaderCardProps) {
+  const t = useI18n();
+  const currentLocale = useCurrentLocale();
+  const isRTL = isLocaleRTL(currentLocale);
+
+  return (
+    <Card className={classes.header}>
+      <Flex align="center" justify={isRTL ? "space-between" : "flex-start"} gap="md" direction={isRTL ? "row" : "row"}>
+        <ThemeIcon
+          size="xl"
+          radius="md"
+          variant="gradient"
+          gradient={{
+            deg: 0,
+            from: "var(--mantine-color-blue-4)",
+            to: "var(--mantine-color-blue-9)",
+          }}
+        >
+          {getIcon(props.headerType)}
+        </ThemeIcon>
+        <Text size="xl" fw={500} dir={isRTL ? "rtl" : "ltr"}>
+          {isRTL
+            ? `${props.value} : ${t(`kubernetes.cluster.${props.headerType}`)}`
+            : `${t(`kubernetes.cluster.${props.headerType}`)} : ${props.value}`}
+        </Text>
+      </Flex>
+    </Card>
+  );
+}
+
+function getIcon(headerType: HeaderTypes) {
+  switch (headerType) {
+    case "providers":
+      return <IconCloudShare size={28} stroke={1.5} />;
+    case "version":
+      return <IconBrandGit size={28} stroke={1.5} />;
+    default:
+      return <IconGeometry size={28} stroke={1.5} />;
+  }
+}
---
+++ apps/nextjs/src/app/[locale]/manage/tools/kubernetes/configmaps/page.tsx
@@ -0,0 +1,27 @@
+import { notFound } from "next/navigation";
+import { Stack, Title } from "@mantine/core";
+
+import { api } from "@homarr/api/server";
+import { auth } from "@homarr/auth/next";
+import { env } from "@homarr/docker/env";
+
+import { ConfigmapsTable } from "~/app/[locale]/manage/tools/kubernetes/configmaps/configmaps-table";
+import { DynamicBreadcrumb } from "~/components/navigation/dynamic-breadcrumb";
+
+export default async function ConfigMapsPage() {
+  const session = await auth();
+  if (!(session?.user.permissions.includes("admin") && env.ENABLE_KUBERNETES)) {
+    notFound();
+  }
+
+  const configMaps = await api.kubernetes.configMaps.getConfigMaps();
+  return (
+    <>
+      <DynamicBreadcrumb />
+      <Stack>
+        <Title order={1}>ConfigMaps</Title>
+        <ConfigmapsTable initialConfigMaps={configMaps} />
+      </Stack>
+    </>
+  );
+}
