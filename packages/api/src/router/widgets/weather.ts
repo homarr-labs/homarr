c19a4009f37ec2aba8d4edd2d19819605ecb9575
Yossi Hillali <hillaliy@me.com>
2025-01-29 23:02:13 +0200

    feat: weather widget - add wind speed, option to disable decimals for temperature & redesign dropdown forecast (#2099)

--- packages/api/src/router/widgets/weather.ts
+++ packages/api/src/router/widgets/weather.ts
@@ -6,7 +6,7 @@ import { createTRPCRouter, publicProcedure } from "../../trpc";
 export const weatherRouter = createTRPCRouter({
   atLocation: publicProcedure.input(validation.widget.weather.atLocationInput).query(async ({ input }) => {
     const res = await fetchWithTimeout(
-      `https://api.open-meteo.com/v1/forecast?latitude=${input.latitude}&longitude=${input.longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=auto`,
+      `https://api.open-meteo.com/v1/forecast?latitude=${input.latitude}&longitude=${input.longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,wind_speed_10m_max,wind_gusts_10m_max&current_weather=true&timezone=auto`,
     );
     const json: unknown = await res.json();
     const weather = await validation.widget.weather.atLocationOutput.parseAsync(json);
@@ -18,6 +18,10 @@ export const weatherRouter = createTRPCRouter({
           weatherCode: weather.daily.weathercode[index] ?? 404,
           maxTemp: weather.daily.temperature_2m_max[index],
           minTemp: weather.daily.temperature_2m_min[index],
+          sunrise: weather.daily.sunrise[index],
+          sunset: weather.daily.sunset[index],
+          maxWindSpeed: weather.daily.wind_speed_10m_max[index],
+          maxWindGusts: weather.daily.wind_gusts_10m_max[index],
         };
       }),
     };
