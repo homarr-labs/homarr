5f36d8b12528fd2c8c3da783b266d614203ec46a
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-01-31 23:32:08 +0100

    feat: improve consistency and design (#1867)
    Co-authored-by: Meier Lukas <meierschlumpf@gmail.com>

--- apps/nextjs/src/app/[locale]/manage/apps/_form.tsx
+++ apps/nextjs/src/app/[locale]/manage/apps/_form.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { useRef } from "react";
 import Link from "next/link";
 import { Button, Group, Stack, Textarea, TextInput } from "@mantine/core";
 import type { z } from "zod";
 
 import { useZodForm } from "@homarr/form";
-import type { TranslationFunction } from "@homarr/translation";
 import { useI18n } from "@homarr/translation/client";
 import { validation } from "@homarr/validation";
 
@@ -14,14 +14,21 @@ import { IconPicker } from "~/components/icons/picker/icon-picker";
 type FormType = z.infer<typeof validation.app.manage>;
 
 interface AppFormProps {
-  submitButtonTranslation: (t: TranslationFunction) => string;
+  buttonLabels: {
+    submit: string;
+    submitAndCreateAnother?: string;
+  };
   initialValues?: FormType;
-  handleSubmit: (values: FormType) => void;
+  handleSubmit: (values: FormType, redirect: boolean, afterSuccess?: () => void) => void;
   isPending: boolean;
 }
 
-export const AppForm = (props: AppFormProps) => {
-  const { submitButtonTranslation, handleSubmit, initialValues, isPending } = props;
+export const AppForm = ({
+  buttonLabels,
+  handleSubmit: originalHandleSubmit,
+  initialValues,
+  isPending,
+}: AppFormProps) => {
   const t = useI18n();
 
   const form = useZodForm(validation.app.manage, {
@@ -33,11 +40,23 @@ export const AppForm = (props: AppFormProps) => {
     },
   });
 
+  const shouldCreateAnother = useRef(false);
+  const handleSubmit = (values: FormType) => {
+    const redirect = !shouldCreateAnother.current;
+    const afterSuccess = shouldCreateAnother.current
+      ? () => {
+          form.reset();
+          shouldCreateAnother.current = false;
+        }
+      : undefined;
+    originalHandleSubmit(values, redirect, afterSuccess);
+  };
+
   return (
     <form onSubmit={form.onSubmit(handleSubmit)}>
       <Stack>
         <TextInput {...form.getInputProps("name")} withAsterisk label={t("app.field.name.label")} />
-        <IconPicker initialValue={initialValues?.iconUrl} {...form.getInputProps("iconUrl")} />
+        <IconPicker {...form.getInputProps("iconUrl")} />
         <Textarea {...form.getInputProps("description")} label={t("app.field.description.label")} />
         <TextInput {...form.getInputProps("href")} label={t("app.field.url.label")} />
 
@@ -45,8 +64,19 @@ export const AppForm = (props: AppFormProps) => {
           <Button variant="default" component={Link} href="/manage/apps">
             {t("common.action.backToOverview")}
           </Button>
+          {buttonLabels.submitAndCreateAnother && (
+            <Button
+              type="submit"
+              onClick={() => {
+                shouldCreateAnother.current = true;
+              }}
+              loading={isPending}
+            >
+              {buttonLabels.submitAndCreateAnother}
+            </Button>
+          )}
           <Button type="submit" loading={isPending}>
-            {submitButtonTranslation(t)}
+            {buttonLabels.submit}
           </Button>
         </Group>
       </Stack>
