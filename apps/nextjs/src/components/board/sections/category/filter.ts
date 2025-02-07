3222759f480e967f7133d99376cf4ca43bfc3baa
Meier Lukas <meierschlumpf@gmail.com>
2025-02-07 21:58:47 +0100

    fix: typecheck issue

--- apps/nextjs/src/components/board/sections/category/filter.ts
+++ apps/nextjs/src/components/board/sections/category/filter.ts
@@ -1,14 +1,23 @@
 import type { WidgetKind } from "@homarr/definitions";
+import type { SettingsContextProps } from "@homarr/settings";
 import type { WidgetComponentProps } from "@homarr/widgets";
 import { reduceWidgetOptionsWithDefaultValues } from "@homarr/widgets";
 
 import type { Item } from "~/app/[locale]/boards/_types";
 
-export const filterByItemKind = <TKind extends WidgetKind>(items: Item[], kind: TKind) => {
+export const filterByItemKind = <TKind extends WidgetKind>(
+  items: Item[],
+  settings: SettingsContextProps,
+  kind: TKind,
+) => {
   return items
     .filter((item) => item.kind === kind)
     .map((item) => ({
       ...item,
-      options: reduceWidgetOptionsWithDefaultValues(kind, item.options) as WidgetComponentProps<TKind>["options"],
+      options: reduceWidgetOptionsWithDefaultValues(
+        kind,
+        settings,
+        item.options,
+      ) as WidgetComponentProps<TKind>["options"],
     }));
 };
