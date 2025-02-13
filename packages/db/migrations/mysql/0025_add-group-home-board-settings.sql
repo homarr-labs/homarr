74ff67479a26ad7e9c3edb64e8d431937c2eb0da
Meier Lukas <meierschlumpf@gmail.com>
2025-02-13 21:56:11 +0100

    fix: update of position of group not working

--- packages/db/migrations/mysql/0025_add-group-home-board-settings.sql
+++ packages/db/migrations/mysql/0025_add-group-home-board-settings.sql
@@ -6,7 +6,17 @@ ALTER TABLE `group` ADD `position` smallint;
 --> statement-breakpoint
 UPDATE `group` SET `position` = -1 WHERE `name` = 'everyone';
 --> statement-breakpoint
-UPDATE `group` SET `position` = ROW_NUMBER() OVER(ORDER BY `name`) WHERE `name` != 'everyone';
+CREATE TABLE `temp_group` (
+    `id` varchar(64) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `position` smallint NOT NULL
+);
+--> statement-breakpoint
+INSERT INTO `temp_group`(`id`, `name`, `position`) SELECT `id`, `name`, ROW_NUMBER() OVER(ORDER BY `name`) FROM `group` WHERE `name` != 'everyone';
+--> statement-breakpoint
+UPDATE `group` SET `position`=(SELECT `position` FROM `temp_group` WHERE `temp_group`.`id`=`group`.`id`);
+--> statement-breakpoint
+DROP TABLE `temp_group`;
 --> statement-breakpoint
 ALTER TABLE `group` MODIFY `position` smallint NOT NULL;
 --> statement-breakpoint
