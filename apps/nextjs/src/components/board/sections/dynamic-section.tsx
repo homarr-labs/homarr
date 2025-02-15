a9c41d398d8be0e0be8ef6812e7ef730c7430527
Jack Weller <97435840+jackwellerreal@users.noreply.github.com>
2025-02-15 16:35:36 +1000

    fix: make checking for borderColor more robust

--- apps/nextjs/src/components/board/sections/dynamic-section.tsx
+++ apps/nextjs/src/components/board/sections/dynamic-section.tsx
@@ -30,7 +30,7 @@ export const BoardDynamicSection = ({ section }: Props) => {
           root: {
             "--opacity": board.opacity / 100,
             overflow: "hidden",
-            borderColor: options.borderColor + " !important",
+            borderColor: options.borderColor ? `${options.borderColor} !important` : undefined,
           },
         }}
         p={0}
