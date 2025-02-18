08f7f6537accc767887e5b95d7dd7a63cd0ea351
Meier Lukas <meierschlumpf@gmail.com>
2025-02-18 20:26:33 +0100

    fix: hydration issue for different layouts

--- apps/nextjs/src/app/[locale]/boards/(content)/_creator.tsx
+++ apps/nextjs/src/app/[locale]/boards/(content)/_creator.tsx
@@ -13,7 +13,7 @@ import { getI18n } from "@homarr/translation/server";
 import { createMetaTitle } from "~/metadata";
 import { createBoardLayout } from "../_layout-creator";
 import type { Board } from "../_types";
-import { ClientBoard } from "./_client";
+import { DynamicClientBoard } from "./_dynamic-client";
 import { BoardContentHeaderActions } from "./_header-actions";
 
 export type Params = Record<string, unknown>;
@@ -37,7 +37,7 @@ export const createBoardContentPage = <TParams extends Record<string, unknown>>(
 
       return (
         <IntegrationProvider integrations={integrations}>
-          <ClientBoard />
+          <DynamicClientBoard />
         </IntegrationProvider>
       );
     },
