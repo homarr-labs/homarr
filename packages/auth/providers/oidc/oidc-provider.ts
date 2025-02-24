a7dd99df3a60b1e045da80f8682b9ea3518e8a8c
Meier Lukas <meierschlumpf@gmail.com>
2025-02-24 17:44:37 +0100

    feat: use fetch with trusted certificates for oidc

--- packages/auth/providers/oidc/oidc-provider.ts
+++ packages/auth/providers/oidc/oidc-provider.ts
@@ -1,7 +1,9 @@
 import type { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
+import { customFetch } from "@auth/core";
 import type { OIDCConfig } from "@auth/core/providers";
 import type { Profile } from "@auth/core/types";
 
+import { fetchWithTrustedCertificatesAsync } from "../../../certificates/src/server";
 import { env } from "../../env";
 import { createRedirectUri } from "../../redirect";
 
@@ -35,6 +37,10 @@ export const OidcProvider = (headers: ReadonlyHeaders | null): OIDCConfig<Profil
       provider: "oidc",
     };
   },
+  // The type for fetch is not identical, but for what we need it it's okay to not be an 1:1 match
+  // See documentation https://authjs.dev/guides/corporate-proxy?framework=next-js
+  // @ts-expect-error `undici` has a `duplex` option
+  [customFetch]: fetchWithTrustedCertificatesAsync,
 });
 
 export const extractProfileName = (profile: Profile) => {
