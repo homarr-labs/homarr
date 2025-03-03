35a73513f6e2adb267c2d173b0adcc8e1ccc050f
Jack Weller <97435840+jackwellerreal@users.noreply.github.com>
2025-03-03 21:27:55 +1000

    feat: hide title and icon when not defined

--- packages/widgets/src/custom-api/component.tsx
+++ packages/widgets/src/custom-api/component.tsx
@@ -9,16 +9,24 @@ import type { WidgetComponentProps } from "../definition";
 
 export default function CustomApiWidget({ options }: WidgetComponentProps<"customApi">) {
   const [{ data }] = clientApi.widget.customApi.fetchURL.useSuspenseQuery({ url: options.url });
-  const value = selectn(options.filter, data) as unknown;
+  const value = options.filter ? selectn(options.filter, data) as unknown : "Please enter a filter";
 
   return (
     <Center h="100%" w="100%">
       <Stack align="center" justify="center" gap="md">
         <Flex direction="row" align="center" gap="lg">
-          <Text ta="center" fw="bold" size="lg">
-            {options.title}
-          </Text>
-          <Image src={options.icon} w="lg" h="lg" />
+          {
+            options.title && (
+              <Text ta="center" fw="bold" size="lg">
+                {options.title}
+              </Text>
+            )
+          }
+          {
+            options.icon && (
+              <Image src={options.icon} w="lg" h="lg" />
+            )
+          }
         </Flex>
         <Text ta="center">{`${value}`}</Text>
       </Stack>
