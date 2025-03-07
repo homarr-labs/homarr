f9571ac916b405bacfff6c32c6ff6cf8a4749f69
Meier Lukas <meierschlumpf@gmail.com>
2025-03-07 22:48:59 +0100

    fix: typecheck issue

--- apps/nextjs/src/components/board/items/actions/test/mocks/item-mock.ts
+++ apps/nextjs/src/components/board/items/actions/test/mocks/item-mock.ts
@@ -14,6 +14,7 @@ export class ItemMockBuilder {
       integrationIds: [],
       advancedOptions: {
         customCssClasses: [],
+        borderColor: "",
       },
       ...item,
     } satisfies Item;
