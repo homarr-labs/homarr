11b8c5c4d70ed0f446e2a4d6079038c513ea1a5e
Jack Weller <97435840+jackwellerreal@users.noreply.github.com>
2025-03-12 17:40:39 +1000

    fix: formatting

--- packages/request-handler/src/stock-price.ts
+++ packages/request-handler/src/stock-price.ts
@@ -9,7 +9,9 @@ export const fetchStockPriceHandler = createCachedWidgetRequestHandler({
   queryKey: "fetchStockPriceResult",
   widgetKind: "stockPrice",
   async requestAsync(input: { stock: string; timeRange: string; timeInterval: string }) {
-    const response = await fetchWithTimeout(`https://query1.finance.yahoo.com/v8/finance/chart/${input.stock}?range=${input.timeRange}&interval=${input.timeInterval}`);
+    const response = await fetchWithTimeout(
+      `https://query1.finance.yahoo.com/v8/finance/chart/${input.stock}?range=${input.timeRange}&interval=${input.timeInterval}`,
+    );
     return responseSchema.parse(await response.json()) as object;
   },
   cacheDuration: dayjs.duration(5, "minutes"),
