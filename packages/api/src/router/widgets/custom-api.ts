d1a08b6483e9d9ac7dba1be05c8bb2926bcf8d9b
Jack Weller <97435840+jackwellerreal@users.noreply.github.com>
2025-03-15 13:23:58 +1000

    fix: check for missing variables

--- packages/api/src/router/widgets/custom-api.ts
+++ packages/api/src/router/widgets/custom-api.ts
@@ -4,7 +4,7 @@ import { fetchCustomApiRequestHandler } from "@homarr/request-handler/custom-api
 
 import { createTRPCRouter, publicProcedure } from "../../trpc";
 
-const serverStatusInputSchema = z.object({
+const fetchCustomApiInputSchema = z.object({
   url: z.string().nonempty(),
   method: z.string().nonempty(),
   headerName: z.string(),
@@ -12,9 +12,10 @@ const serverStatusInputSchema = z.object({
 });
 
 export const customApiRouter = createTRPCRouter({
-  fetchURL: publicProcedure.input(serverStatusInputSchema).query(async ({ input }) => {
+  fetchURL: publicProcedure.input(fetchCustomApiInputSchema).query(async ({ input }) => {
     const innerHandler = fetchCustomApiRequestHandler.handler({
       url: input.url,
+      method: input.method,
       headerName: input.headerName,
       headerValue: input.headerValue,
     });
--- packages/widgets/src/custom-api/component.tsx
+++ packages/widgets/src/custom-api/component.tsx
@@ -1,33 +1,52 @@
 "use client";
 
-import { Center, Flex, Image, Stack, Text } from "@mantine/core";
+import { Center, Flex, Image, Stack, Text, Title } from "@mantine/core";
 import selectn from "selectn";
 
 import { clientApi } from "@homarr/api/client";
 
 import type { WidgetComponentProps } from "../definition";
 
-export default function CustomApiWidget({ options }: WidgetComponentProps<"customApi">) {
-  const [{ data }] = clientApi.widget.customApi.fetchURL.useSuspenseQuery({ url: options.url, method: options.method,headerName: options.headerName, headerValue: options.headerValue });
-  const value = options.filter ? selectn(options.filter as string, data) as unknown : "Please enter a filter";
+export default function CustomApiWidget({ options, width }: WidgetComponentProps<"customApi">) {
+  if (!options.url) {
+    throw new Error("URL is required");
+  }
+
+  if (!options.filter) {
+    throw new Error("Filter is required");
+  }
+
+  const [{ data }] = clientApi.widget.customApi.fetchURL.useSuspenseQuery({
+    url: options.url,
+    method: options.method,
+    headerName: options.headerName,
+    headerValue: options.headerValue,
+  });
+
+  const value = selectn(options.filter, data) as unknown;
 
   return (
     <Center h="100%" w="100%">
+      {options.title && (
+        <Title pos="absolute" top={10} left={10} ta="center" order={width > 280 ? 3 : 4}>
+          {options.title}
+        </Title>
+      )}
+      {options.icon && (
+        <Image
+          src={String(options.icon)}
+          pos="absolute"
+          top={10}
+          right={10}
+          w={width > 280 ? "xl" : "lg"}
+          h={width > 280 ? "xl" : "lg"}
+          style={{
+            borderRadius: "100%",
+          }}
+        />
+      )}
       <Stack align="center" justify="center" gap="md">
-        <Flex direction="row" align="center" gap="lg">
-          {
-            options.title && (
-              <Text ta="center" fw="bold" size="lg">
-                {options.title}
-              </Text>
-            )
-          }
-          {
-            options.icon && (
-              <Image src={String(options.icon)} w="lg" h="lg" />
-            )
-          }
-        </Flex>
+        <Flex direction="row" align="center" gap="lg"></Flex>
         <Text ta="center">{String(value)}</Text>
       </Stack>
     </Center>
