287751d049eec41ee0a7cef83502ccf82cb784e9
Aandree5 <andre-silva5@hotmail.com>
2025-03-21 16:47:47 +0000

    Updated UI

--- packages/widgets/src/releases/component.tsx
+++ packages/widgets/src/releases/component.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Divider, Grid, Group, Stack, Text } from "@mantine/core";
-import { useFormatter } from "next-intl";
+import { Divider, Grid, Stack, Text } from "@mantine/core";
+import { useFormatter, useNow } from "next-intl";
 
 import { clientApi } from "@homarr/api/client";
 import { useRequiredBoard } from "@homarr/boards/context";
@@ -9,7 +9,7 @@ import { useScopedI18n } from "@homarr/translation/client";
 import { MaskedOrNormalImage } from "@homarr/ui";
 
 import type { WidgetComponentProps } from "../definition";
-import classes from "./component.module.scss";
+// import classes from "./component.module.scss";
 import type { ReleaseRepository } from "./release-repository";
 
 function isDateWithin(date: Date, relativeDate: string): boolean {
@@ -44,6 +44,7 @@ function isDateWithin(date: Date, relativeDate: string): boolean {
 
 export default function ReleasesWidget({ options }: WidgetComponentProps<"releases">) {
   const t = useScopedI18n("widget.releases");
+  const now = useNow();
   const formatter = useFormatter();
   const board = useRequiredBoard();
 
@@ -82,84 +83,67 @@ export default function ReleasesWidget({ options }: WidgetComponentProps<"releas
     ? options.repositories.filter((repository) => repository.shouldHighlight)
     : options.repositories;
 
-  return (
-    <Stack>
-      <Stack gap={2}>
-        {repositories.map((repository: ReleaseRepository) => (
-          <Stack key={`${repository.provider.name}.${repository.identifier}`} gap={5}>
-            <Grid columns={2} p="xs" gutter="xs" align="center">
-              <Grid.Col span="content">
-                <MaskedOrNormalImage
-                  imageUrl={repository.iconUrl ?? repository.provider.iconUrl}
-                  hasColor={board.iconColor !== null}
-                  style={{
-                    height: "1.7em",
-                    width: "1.7em",
-                  }}
-                />
-              </Grid.Col>
-              <Grid.Col span="auto">
-                <Grid className={classes.card} p={0} gutter="xs">
-                  <Grid.Col span={8} p={0}>
-                    <Text truncate="end" size="sm">
-                      {repository.identifier}
+  if (options.sortBy === "releaseDate") {
+    repositories.sort((a, b) => {
+      if (a.latestReleaseDate === undefined) return 1;
+      if (b.latestReleaseDate === undefined) return -1;
+      return a.latestReleaseDate > b.latestReleaseDate ? -1 : 1;
+    });
+
+    return (
+      <Stack>
+        <Stack gap={2}>
+          {repositories.map((repository: ReleaseRepository) => (
+            <Stack key={`${repository.provider.name}.${repository.identifier}`} gap={0}>
+              <Grid columns={17} p={5} gutter="xs" justify="stretch" align="center">
+                <Grid.Col span={1.2}>
+                  <MaskedOrNormalImage
+                    imageUrl={repository.iconUrl ?? repository.provider.iconUrl}
+                    hasColor={board.iconColor !== null}
+                    style={{
+                      width: "100%",
+                      aspectRatio: "1/1",
+                    }}
+                  />
+                </Grid.Col>
+                <Grid.Col span={6} p={0}>
+                  <Text truncate="end" size="xs">
+                    {repository.identifier}
+                  </Text>
+                </Grid.Col>
+                <Grid.Col span={5.8} p={0}>
+                  <Text
+                    size="xs"
+                    fw={700}
+                    style={{
+                      textAlign: "right",
+                    }}
+                  >
+                    {repository.latestRelease ?? t("not-found")}
+                  </Text>
+                </Grid.Col>
+                <Grid.Col span={1} p={0}>
+                  {repository.shouldHighlight && (
+                    <Text c="primaryColor" fw={600} style={{ textAlign: "center" }}>
+                      !
                     </Text>
-                  </Grid.Col>
-                  <Grid.Col span={4} p={0}>
-                    <Text
-                      size="sm"
-                      fw={700}
-                      c="primaryColor"
-                      style={{
-                        textAlign: "right",
-                      }}
-                    >
-                      {repository.latestRelease ?? t("not-found")}
-                    </Text>
-                  </Grid.Col>
-                  <Grid.Col span={8} p={0}>
-                    <Group gap={5}>
-                      <MaskedOrNormalImage
-                        imageUrl={repository.provider.iconUrl}
-                        color="dimmed"
-                        hasColor={true}
-                        style={{
-                          height: "0.8em",
-                          width: "0.8em",
-                        }}
-                      />
-                      <Text c="dimmed" size="xs">
-                        {repository.provider.name}
-                      </Text>
-                    </Group>
-                  </Grid.Col>
-                  <Grid.Col span={4} p={0}>
-                    <Group justify="end" gap={5}>
-                      <Text
-                        size="sm"
-                        style={{
-                          textAlign: "right",
-                        }}
-                      >
-                        {repository.latestReleaseDate &&
-                          formatter.relativeTime(repository.latestReleaseDate, {
-                            style: "narrow",
-                          })}
-                      </Text>
-                      {repository.shouldHighlight && (
-                        <Text c="dimmed" fw={600}>
-                          !
-                        </Text>
-                      )}
-                    </Group>
-                  </Grid.Col>
-                </Grid>
-              </Grid.Col>
-            </Grid>
-            <Divider />
-          </Stack>
-        ))}
+                  )}
+                </Grid.Col>
+                <Grid.Col span={3} p={0}>
+                  <Text size="xs" c="dimmed">
+                    {repository.latestReleaseDate &&
+                      formatter.relativeTime(repository.latestReleaseDate, {
+                        now,
+                        style: "narrow",
+                      })}
+                  </Text>
+                </Grid.Col>
+              </Grid>
+              <Divider />
+            </Stack>
+          ))}
+        </Stack>
       </Stack>
-    </Stack>
-  );
+    );
+  }
 }
