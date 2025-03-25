779d87a79fd4e150698829de6cbdf403bf4445eb
Meier Lukas <meierschlumpf@gmail.com>
2025-03-25 17:45:14 +0100

    fix(auth): fusion auth returns www-authenticate token response

--- packages/auth/providers/oidc/oidc-provider.ts
+++ packages/auth/providers/oidc/oidc-provider.ts
@@ -22,6 +22,24 @@ export const OidcProvider = (headers: ReadonlyHeaders | null): OIDCConfig<Profil
       redirect_uri: createRedirectUri(headers, "/api/auth/callback/oidc", "https"),
     },
   },
+  token: {
+    // Providers like fusionauth may return ww-authenticate which results in an error
+    // https://github.com/nextauthjs/next-auth/issues/8745
+    // https://github.com/homarr-labs/homarr/issues/2690
+    conform: (response: Response) => {
+      if (response.status === 401) return response;
+
+      const newHeaders = Array.from(response.headers.entries())
+        .filter(([key]) => key.toLowerCase() !== "www-authenticate")
+        .reduce((headers, [key, value]) => (headers.append(key, value), headers), new Headers());
+
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: newHeaders,
+      });
+    },
+  },
   profile(profile) {
     if (!profile.sub) {
       throw new Error(`OIDC provider did not return a sub property='${Object.keys(profile).join(",")}'`);
