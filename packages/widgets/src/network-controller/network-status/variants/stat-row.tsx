57f43041d41589c0d93bfe5db547031eddb95e66
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-03-25 21:54:27 +0100

    fix: pr feedback

--- packages/widgets/src/network-controller/network-status/variants/stat-row.tsx
+++ packages/widgets/src/network-controller/network-status/variants/stat-row.tsx
@@ -3,7 +3,7 @@ import { Stack, Text } from "@mantine/core";
 export const StatRow = ({ label, value }: { label: string; value: string | number }) => {
   return (
     <Stack gap={0}>
-      <Text size={"xl2"} fw={900} lh={1}>
+      <Text size={"2xl"} fw={900} lh={1}>
         {value}
       </Text>
       <Text size={"md"} c={"dimmed"}>
