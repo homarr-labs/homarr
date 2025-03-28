08e0dd76ae981e5597980ff6002a4c757d07d0a1
Meier Lukas <meierschlumpf@gmail.com>
2025-03-28 13:39:20 +0100

    chore(deps): update nextjs to 15.2.3 (#2701)
    * chore(deps): update nextjs to 15.2.3
    
    * fix: dev server crashes with next 15.2.0
    
    * chore(deps): reenable updates for next packages
    
    * chore: address pull request feedback

---
+++ packages/cron-job-runner/src/register.ts
@@ -0,0 +1,12 @@
+import { jobGroup } from "@homarr/cron-jobs";
+
+import { cronJobRunnerChannel } from ".";
+
+/**
+ * Registers the cron job runner to listen to the Redis PubSub channel.
+ */
+export const registerCronJobRunner = () => {
+  cronJobRunnerChannel.subscribe((jobName) => {
+    jobGroup.runManually(jobName);
+  });
+};
