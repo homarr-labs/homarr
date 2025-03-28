62c7955e48ca62b4bf1baf26ac6a12847795ac18
Meier Lukas <meierschlumpf@gmail.com>
2025-03-28 17:44:11 +0100

    fix(import): autofix missing shapes for sidebars and some sections as well (#2723)

--- packages/old-import/src/import-error.ts
+++ packages/old-import/src/import-error.ts
@@ -1,15 +1,9 @@
-import type { BoardSize, OldmarrConfig } from "@homarr/old-schema";
+import type { OldmarrConfig } from "@homarr/old-schema";
 
 export class OldHomarrImportError extends Error {
   constructor(oldConfig: OldmarrConfig, cause: unknown) {
     super(`Failed to import old homarr configuration name=${oldConfig.configProperties.name}`, {
       cause,
     });
-  }
-}
-
-export class OldHomarrScreenSizeError extends Error {
-  constructor(type: "app" | "widget", id: string, screenSize: BoardSize) {
-    super(`Screen size not found for type=${type} id=${id} screenSize=${screenSize}`);
   }
 }
