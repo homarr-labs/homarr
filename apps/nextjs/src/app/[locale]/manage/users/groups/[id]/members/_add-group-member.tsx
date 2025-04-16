00053bda7bbcaaf359074c3b35573ee0afc1409d
Thomas Camlong <thomas@ajnart.fr>
2025-04-16 11:34:33 +0200

    feat: Prefer primary color (#2665)

--- apps/nextjs/src/app/[locale]/manage/users/groups/[id]/members/_add-group-member.tsx
+++ apps/nextjs/src/app/[locale]/manage/users/groups/[id]/members/_add-group-member.tsx
@@ -40,9 +40,5 @@ export const AddGroupMember = ({ groupId, presentUserIds }: AddGroupMemberProps)
     );
   }, [openModal, presentUserIds, groupId, mutateAsync, tMembersAdd]);
 
-  return (
-    <MobileAffixButton color="teal" onClick={handleAddMember}>
-      {tMembersAdd("label")}
-    </MobileAffixButton>
-  );
+  return <MobileAffixButton onClick={handleAddMember}>{tMembersAdd("label")}</MobileAffixButton>;
 };
