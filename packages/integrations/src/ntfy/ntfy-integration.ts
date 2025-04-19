182461a9961f1b401abcd358b2a7db709cee1060
Meow <me@itsmeow.cat>
2025-04-19 02:59:07 -0400

    fix(ntfy): fix notification fetching

--- packages/integrations/src/ntfy/ntfy-integration.ts
+++ packages/integrations/src/ntfy/ntfy-integration.ts
@@ -2,20 +2,13 @@ import { fetchWithTrustedCertificatesAsync } from "@homarr/certificates/server";
 
 import type { Notification } from "../interfaces/notifications/notification";
 import { NotificationsIntegration } from "../interfaces/notifications/notifications-integration";
-import { ntfyNotificationPollSchema } from "./ntfy-schema";
+import { ntfyNotificationSchema } from "./ntfy-schema";
 
 export class NTFYIntegration extends NotificationsIntegration {
   public async testConnectionAsync(): Promise<void> {
     await super.handleTestConnectionResponseAsync({
       queryFunctionAsync: async () => {
-        return await fetchWithTrustedCertificatesAsync(
-          this.url("/v1/account"),
-          this.hasSecretValue("apiKey")
-            ? {
-                headers: { Authorization: `Bearer ${super.getSecretValue("apiKey")}` },
-              }
-            : {},
-        );
+        return await fetchWithTrustedCertificatesAsync(this.url("/v1/account"), { headers: this.getHeaders() });
       },
     });
   }
@@ -23,14 +16,17 @@ export class NTFYIntegration extends NotificationsIntegration {
   private getTopicURL(topic: string) {
     return this.url(`/${topic}/json`, { poll: 1 });
   }
+  private getHeaders() {
+    return this.hasSecretValue("apiKey") ? { Authorization: `Bearer ${super.getSecretValue("apiKey")}` } : {};
+  }
 
   public async getNotificationsAsync(topics: string[]) {
     const notifications = await Promise.all(
       topics.map(async (topic) => {
         const url = this.getTopicURL(topic);
-        return await ntfyNotificationPollSchema.parseAsync(
+        return await Promise.all(
           (
-            await fetchWithTrustedCertificatesAsync(url)
+            await fetchWithTrustedCertificatesAsync(url, { headers: this.getHeaders() })
               .then((response) => {
                 if (!response.ok) {
                   throw new Error(response.statusText);
@@ -45,20 +41,31 @@ export class NTFYIntegration extends NotificationsIntegration {
                 }
               })
           )
+            .trim()
             // response is provided as individual lines of JSON
             .split("\n")
-            .map((line) => JSON.parse(line) as unknown),
+            .map(async (line) => {
+              const json = JSON.parse(line) as unknown;
+              const parsed = await ntfyNotificationSchema.safeParseAsync(json);
+              if (parsed.success) return parsed.data;
+              // ignore invalid formatted messages
+              else return null;
+            }),
         );
       }),
     );
 
-    return notifications.flat(1).map((notification): Notification => {
-      return {
-        id: notification.id,
-        time: new Date(notification.time),
-        title: notification.title,
-        body: notification.message,
-      };
-    });
+    return notifications
+      .flat(1)
+      .filter((notification) => notification !== null)
+      .map((notification): Notification => {
+        const topicURL = this.url(`/${notification.topic}`);
+        return {
+          id: notification.id,
+          time: new Date(notification.time),
+          title: notification.title ?? topicURL.hostname + topicURL.pathname,
+          body: notification.message,
+        };
+      });
   }
 }
--- packages/integrations/src/ntfy/ntfy-schema.ts
+++ packages/integrations/src/ntfy/ntfy-schema.ts
@@ -2,13 +2,11 @@ import { z } from "zod";
 
 // There are more properties, see: https://docs.ntfy.sh/subscribe/api/#json-message-format
 // Not all properties are required for this use case.
-export const ntfyNotificationPollSchema = z.array(
-  z.object({
-    id: z.string(),
-    time: z.coerce.date(),
-    event: z.literal("message"), // we only care about messages
-    topic: z.string(),
-    title: z.string(),
-    message: z.string(),
-  }),
-);
+export const ntfyNotificationSchema = z.object({
+  id: z.string(),
+  time: z.coerce.date(),
+  event: z.literal("message"), // we only care about messages
+  topic: z.string(),
+  title: z.optional(z.string()),
+  message: z.string(),
+});
