1151dd9c79d72aa7503a08cd1b898c34b0a5d860
Aandree5 <andre-silva5@hotmail.com>
2025-05-16 20:41:38 +0100

    feat(releases-widget): add `import from docker` functionality

--- packages/widgets/src/releases/component.tsx
+++ packages/widgets/src/releases/component.tsx
@@ -150,7 +150,7 @@ export default function ReleasesWidget({ options }: WidgetComponentProps<"releas
               onClick={() => toggleExpandedRepository(repository)}
             >
               <MaskedOrNormalImage
-                imageUrl={repository.iconUrl ?? Providers[repository.providerKey]?.iconUrl}
+                imageUrl={repository.iconUrl ?? Providers[repository.providerKey].iconUrl}
                 hasColor={hasIconColor}
                 style={{
                   width: "1em",
@@ -330,7 +330,7 @@ const ExpandedDisplay = ({ repository, hasIconColor }: ExtendedDisplayProps) => 
         <Group justify="space-between" align="center">
           <Group gap={5} align="center">
             <MaskedOrNormalImage
-              imageUrl={Providers[repository.providerKey]?.iconUrl}
+              imageUrl={Providers[repository.providerKey].iconUrl}
               hasColor={hasIconColor}
               style={{
                 width: "1em",
@@ -338,7 +338,7 @@ const ExpandedDisplay = ({ repository, hasIconColor }: ExtendedDisplayProps) => 
               }}
             />
             <Text size="xs" c="iconColor" ff="monospace">
-              {Providers[repository.providerKey]?.name}
+              {Providers[repository.providerKey].name}
             </Text>
           </Group>
           {repository.createdAt && (
