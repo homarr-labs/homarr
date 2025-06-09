d9f84a5d2ababca282ee09a58d1e9b7be9145f91
Aandree5 <andre-silva5@hotmail.com>
2025-06-09 22:44:26 +0100

    refactor(releases-widget): moved types from `zod` to interfaces
    - Improved `error.code` compile time validation
    - Renamed `error.options` to `error.messages`in tranlations

--- packages/integrations/src/interfaces/releases-providers/releases-providers-types.ts
+++ packages/integrations/src/interfaces/releases-providers/releases-providers-types.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import type { TranslationObject } from "@homarr/translation";
 
 export interface ReleasesVersionFilter {
   prefix?: string;
@@ -6,72 +6,54 @@ export interface ReleasesVersionFilter {
   suffix?: string;
 }
 
-const _detailsProviderSchema = z
-  .object({
-    projectUrl: z.string().optional(),
-    projectDescription: z.string().optional(),
-    isFork: z.boolean().optional(),
-    isArchived: z.boolean().optional(),
-    createdAt: z.date().optional(),
-    starsCount: z.number().optional(),
-    openIssues: z.number().optional(),
-    forksCount: z.number().optional(),
-  })
-  .optional();
+export interface DetailsProviderResponse {
+  projectUrl?: string;
+  projectDescription?: string;
+  isFork?: boolean;
+  isArchived?: boolean;
+  createdAt?: Date;
+  starsCount?: number;
+  openIssues?: number;
+  forksCount?: number;
+}
 
-export type DetailsProviderResponse = z.infer<typeof _detailsProviderSchema>;
+export interface ReleaseProviderResponse {
+  latestRelease: string;
+  latestReleaseAt: Date;
+  releaseUrl?: string;
+  releaseDescription?: string;
+  isPreRelease?: boolean;
+}
 
-const _releaseProviderSchema = z.object({
-  latestRelease: z.string(),
-  latestReleaseAt: z.date(),
-  releaseUrl: z.string().optional(),
-  releaseDescription: z.string().optional(),
-  isPreRelease: z.boolean().optional(),
-});
+export interface ReleasesRepository {
+  providerKey: string;
+  identifier: string;
+  versionFilter?: ReleasesVersionFilter;
+}
 
-export type ReleaseProviderResponse = z.infer<typeof _releaseProviderSchema>;
+type ReleasesErrorKeys = keyof TranslationObject["widget"]["releases"]["error"]["messages"];
 
-const _releasesRepositorySchema = z.object({
-  providerKey: z.string(),
-  identifier: z.string(),
-  versionFilter: z
-    .object({
-      prefix: z.string().optional(),
-      precision: z.number(),
-      suffix: z.string().optional(),
-    })
-    .optional(),
-});
-
-export type ReleasesRepository = z.infer<typeof _releasesRepositorySchema>;
-
-const _releasesReponseSchema = z
-  .object({
-    identifier: z.string(),
-    providerKey: z.string(),
-    latestRelease: z.string().optional(),
-    latestReleaseAt: z.date().optional(),
-    releaseUrl: z.string().optional(),
-    releaseDescription: z.string().optional(),
-    isPreRelease: z.boolean().optional(),
-    projectUrl: z.string().optional(),
-    projectDescription: z.string().optional(),
-    isFork: z.boolean().optional(),
-    isArchived: z.boolean().optional(),
-    createdAt: z.date().optional(),
-    starsCount: z.number().optional(),
-    openIssues: z.number().optional(),
-    forksCount: z.number().optional(),
-  })
-  .or(
-    z.object({
-      identifier: z.string(),
-      providerKey: z.string(),
-      error: z.object({
-        code: z.string().optional(),
-        message: z.string().optional(),
-      }),
-    }),
-  );
-
-export type ReleasesResponse = z.infer<typeof _releasesReponseSchema>;
+export interface ReleasesResponse {
+  identifier: string;
+  providerKey: string;
+  latestRelease?: string;
+  latestReleaseAt?: Date;
+  releaseUrl?: string;
+  releaseDescription?: string;
+  isPreRelease?: boolean;
+  projectUrl?: string;
+  projectDescription?: string;
+  isFork?: boolean;
+  isArchived?: boolean;
+  createdAt?: Date;
+  starsCount?: number;
+  openIssues?: number;
+  forksCount?: number;
+  error?:
+    | {
+        code: ReleasesErrorKeys;
+      }
+    | {
+        message: string;
+      };
+}
