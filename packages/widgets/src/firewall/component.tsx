383ed38b4c6d6cd834a2a0b267e60b85e9f70ca4
oupsman <oupsman@oupsman.fr>
2025-06-11 07:44:57 +0000

    feat : adding OPNsense integration, firewall kind integration and firewall widget
    Styling the widget

--- packages/widgets/src/firewall/component.tsx
+++ packages/widgets/src/firewall/component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ScrollArea, Tabs, RingProgress, Center, Text, Table, TableTbody, TableThead, TableTr, Popover } from "@mantine/core";
+import { ScrollArea, Tabs, RingProgress, Center, Text, Table, TableTbody, TableThead, TableTr, Flex, Accordion } from "@mantine/core";
 import {
   IconCpu,
 } from "@tabler/icons-react";
@@ -39,98 +39,109 @@ export default function FirewallWidget({
   const t = useI18n();
   const isTiny = width < 256;
 
+  function formatBytes(bytes: number, decimals: number = 2): string {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + ' ' + sizes[i];
+  }
   return (
-    <ScrollArea h="100%">
+  <ScrollArea h="100%" >
       {firewallsData.map(({ integration, summary }) => (
         <Tabs key={integration.name} variant="outline">
           <Tabs.List grow>
-            <Tabs.Tab value="system" fz="xs">
-              <b>{t("widget.firewall.tab.system") + " " + integration.name}</b>
-            </Tabs.Tab>
-            <Tabs.Tab value="interfaces" fz="xs">
-              <b>{t("widget.firewall.tab.interfaces") + " " + integration.name}</b>
+            <Tabs.Tab value={integration.name} fz="xs">
+              <b>{integration.name}</b>
             </Tabs.Tab>
           </Tabs.List>
-          <Tabs.Panel value="system">
-            <RingProgress
-              className="firewall-cpu"
-              roundCaps
-              size={isTiny ? 50 : 100}
-              thickness={isTiny ? 4 : 8}
-              label={
-                <Center style={{ flexDirection: "column" }}>
-                  <Text className="firewall-cpu-utilization-value" size={isTiny ? "8px" : "xs"}>
-                    {`${summary.cpu.idle.toFixed(2)}%`}
-                  </Text>
-                  <IconCpu className="firewall-cpu-utilization-icon" size={isTiny ? 8 : 16} />
-                </Center>
-              }
-              sections={[
-                {
-                  value: Number(summary.cpu.idle.toFixed(2)),
-                  color: progressColor(Number(summary.cpu.idle.toFixed(2))),
-                },
-              ]}
-            />
-            <RingProgress
-              className="firewall-memory"
-              roundCaps
-              size={isTiny ? 50 : 100}
-              thickness={isTiny ? 4 : 8}
-              label={
-                <Center style={{ flexDirection: "column" }}>
-                  <Text className="firewall-memory-utilization-value" size={isTiny ? "8px" : "xs"}>
-                    {`${summary.memory.percent.toFixed(2)}%`}
-                  </Text>
-                  <IconCpu className="firewall-memory-utilization-icon" size={isTiny ? 8 : 16} />
-                </Center>
-              }
-              sections={[
-                {
-                  value: Number(summary.memory.percent.toFixed(2)),
-                  color: progressColor(Number(summary.memory.percent.toFixed(2))),
-                },
-              ]}
-            />
-          </Tabs.Panel>
-          <Tabs.Panel value="interfaces">
-            <Table highlightOnHover>
-              <TableThead>
-                <TableTr fz={isTiny ? "8px" : "xs"}>
-                  <Table.Th ta="start" p={0}>
-                    {t("widget.firewall.widget.interfaces.name")}
-                  </Table.Th>
-                    <Table.Th ta="start" p={0}>
-                      {t("widget.firewall.widget.interfaces.trans")}
-                    </Table.Th>
-                    <Table.Th ta="start" p={0}>
-                      {t("widget.firewall.widget.interfaces.recv")}
-                    </Table.Th>
-                </TableTr>
-              </TableThead>
-              <TableTbody>
-                {summary.interfaces
-                  .map((item) => {
-                    return (
-                          <TableTr fz={isTiny ? "8px" : "xs"}>
-                            <td>
-                              {item.name}
-                            </td>
-                            <td style={{ WebkitLineClamp: "1" }}>
-                              {item.trans}
-                            </td>
-                            <td>
-                              {item.recv}
-                            </td>
-                          </TableTr>
-                    );
-                  })}
-              </TableTbody>
-            </Table>
-
+          <Tabs.Panel value={integration.name}>
+            <Flex gap="sm"
+                   className="firewall"
+                   p="sm"
+                   pos="relative"
+            >
+              <Text w="100%" pos="relative" align="center" top={8} left={8}>Version: <br/>{summary.version}</Text>
+              <RingProgress
+                className="firewall-cpu"
+                roundCaps
+                size={isTiny ? 50 : 100}
+                thickness={isTiny ? 4 : 8}
+                pos="relative"
+                label={
+                  <Center style={{ flexDirection: "column" }}>
+                    <Text className="firewall-cpu-utilization-value" size={isTiny ? "8px" : "xs"}>
+                      {`${summary.cpu.idle.toFixed(2)}%`}
+                    </Text>
+                    <IconCpu className="firewall-cpu-utilization-icon" size={isTiny ? 8 : 16} />
+                  </Center>
+                }
+                sections={[
+                  {
+                    value: Number(summary.cpu.idle.toFixed(2)),
+                    color: progressColor(Number(summary.cpu.idle.toFixed(2))),
+                  },
+                ]}
+              />
+              <RingProgress
+                className="firewall-memory"
+                roundCaps
+                size={isTiny ? 50 : 100}
+                thickness={isTiny ? 4 : 8}
+                pos="relative"
+                label={
+                  <Center style={{ flexDirection: "column" }}>
+                    <Text className="firewall-memory-utilization-value" size={isTiny ? "8px" : "xs"}>
+                      {`${summary.memory.percent.toFixed(2)}%`}
+                    </Text>
+                    <IconCpu className="firewall-memory-utilization-icon" size={isTiny ? 8 : 16} />
+                  </Center>
+                }
+                sections={[
+                  {
+                    value: Number(summary.memory.percent.toFixed(2)),
+                    color: progressColor(Number(summary.memory.percent.toFixed(2))),
+                  },
+                ]}
+              />
+            </Flex>
+            <Accordion>
+              <Accordion.Item value="interfaces">
+                <Accordion.Control>{t("widget.firewall.widget.interfaces.title")}</Accordion.Control>
+                <Accordion.Panel>
+                  <Table highlightOnHover>
+                    <TableThead>
+                      <TableTr fz={isTiny ? "8px" : "xs"}>
+                        <Table.Th ta="start" p={0}>
+                          {t("widget.firewall.widget.interfaces.name")}
+                        </Table.Th>
+                        <Table.Th ta="start" p={0}>
+                          {t("widget.firewall.widget.interfaces.trans")}
+                        </Table.Th>
+                        <Table.Th ta="start" p={0}>
+                          {t("widget.firewall.widget.interfaces.recv")}
+                        </Table.Th>
+                      </TableTr>
+                    </TableThead>
+                    <TableTbody>
+                      {summary.interfaces.map((item) => (
+                        <TableTr key={item.name} fz={isTiny ? "8px" : "xs"}>
+                          <td>{item.name}</td>
+                          <td style={{ WebkitLineClamp: "1" }}>{formatBytes(item.trans)}</td>
+                          <td>{formatBytes(item.recv)}</td>
+                        </TableTr>
+                      ))}
+                    </TableTbody>
+                  </Table>
+                </Accordion.Panel>
+              </Accordion.Item>
+            </Accordion>
           </Tabs.Panel>
         </Tabs>
       ))}
     </ScrollArea>
-  );
+  )
 }
