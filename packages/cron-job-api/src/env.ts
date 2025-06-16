de5a6a05c4c4afc9a6f8f7e285b41d0048c5a353
Meier Lukas <meierschlumpf@gmail.com>
2025-06-16 20:49:20 +0200

    fix: generate cron-job api-key for production

--- packages/cron-job-api/src/env.ts
+++ packages/cron-job-api/src/env.ts
@@ -5,7 +5,6 @@ import { createEnv } from "@homarr/env";
 
 export const env = createEnv({
   server: {
-    // TODO: Generate a key in production (e.g. using `openssl rand -base64 32`)
     CRON_JOB_API_KEY: commonEnv.NODE_ENV === "development" ? z.string().default("test") : z.string(),
   },
   experimental__runtimeEnv: process.env,
--- scripts/run.sh
+++ scripts/run.sh
@@ -13,6 +13,8 @@ fi
 
 # Auth secret is generated every time the container starts as it is required, but not used because we don't need JWTs or Mail hashing
 export AUTH_SECRET=$(openssl rand -base64 32)
+# Cron job API key is generated every time the container starts as it is required for communication between nextjs-api and tasks-api
+export CRON_JOB_API_KEY=$(openssl rand -base64 32)
 
 # Start nginx proxy
 # 1. Replace the HOSTNAME in the nginx template file
