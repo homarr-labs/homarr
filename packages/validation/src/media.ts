fb94ab14e3631847fec52ec5f3074111cb38b732
Meier Lukas <meierschlumpf@gmail.com>
2025-06-23 20:05:29 +0200

    fix: migrate superRefine to check for mediaUploadSchema

--- packages/validation/src/media.ts
+++ packages/validation/src/media.ts
@@ -1,41 +1,34 @@
 import { zfd } from "zod-form-data";
-import type { z } from "zod/v4";
 
 import { createCustomErrorParams } from "./form/i18n";
 
 export const supportedMediaUploadFormats = ["image/png", "image/jpeg", "image/webp", "image/gif", "image/svg+xml"];
 
 export const mediaUploadSchema = zfd.formData({
-  file: zfd.file().superRefine((value: File | null, context: z.RefinementCtx) => {
-    if (!value) {
-      return context.addIssue({
-        code: "invalid_type",
-        expected: "object",
-        received: "null",
-      });
-    }
-
-    if (!supportedMediaUploadFormats.includes(value.type)) {
-      return context.addIssue({
+  file: zfd.file().check((context) => {
+    if (!supportedMediaUploadFormats.includes(context.value.type)) {
+      context.issues.push({
         code: "custom",
         params: createCustomErrorParams({
           key: "invalidFileType",
           params: { expected: `one of ${supportedMediaUploadFormats.join(", ")}` },
         }),
+        input: context.value.type,
       });
+      return;
     }
 
-    if (value.size > 1024 * 1024 * 32) {
+    if (context.value.size > 1024 * 1024 * 32) {
       // Don't forget to update the limit in nginx.conf (client_max_body_size)
-      return context.addIssue({
+      context.issues.push({
         code: "custom",
         params: createCustomErrorParams({
           key: "fileTooLarge",
           params: { maxSize: "32 MB" },
         }),
+        input: context.value.size,
       });
+      return;
     }
-
-    return null;
   }),
 });
