1207180d120d3d4977800b40c5fda317dca9a347
Meier Lukas <meierschlumpf@gmail.com>
2025-06-23 21:23:52 +0200

    fix: zod form requires object

--- packages/form/src/index.ts
+++ packages/form/src/index.ts
@@ -1,12 +1,12 @@
 import { useForm } from "@mantine/form";
 import { zod4Resolver } from "mantine-form-zod-resolver";
-import type { ZodType } from "zod/v4";
+import type { ZodObject } from "zod/v4";
 import { z } from "zod/v4";
 
 import { useI18n } from "@homarr/translation/client";
 import { zodErrorMap } from "@homarr/validation/form/i18n";
 
-export const useZodForm = <TSchema extends ZodType>(
+export const useZodForm = <TSchema extends ZodObject>(
   schema: TSchema,
   options: Omit<
     Exclude<Parameters<typeof useForm<z.infer<TSchema>>>[0], undefined>,
