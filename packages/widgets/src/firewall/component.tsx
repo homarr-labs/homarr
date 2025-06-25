2740fceb5559a80256c8b4ab4ce949329b0130c3
oupsman <oupsman@oupsman.fr>
2025-06-25 15:49:43 +0200

    feat: OPNsense integration
    Moving the component functions back to the component file

--- packages/widgets/src/firewall/component.tsx
+++ packages/widgets/src/firewall/component.tsx
@@ -6,14 +6,11 @@ import duration from "dayjs/plugin/duration";
 
 import { useI18n } from "@homarr/translation/client";
 
+import { clientApi } from "@homarr/api/client";
+
 import type { WidgetComponentProps } from "../definition";
-import { calculateBandwidth, formatBitsPerSec } from "./functions";
-import {
-  useUpdatingCpuStatus,
-  useUpdatingInterfacesStatus,
-  useUpdatingMemoryStatus,
-  useUpdatingVersionStatus,
-} from "./hooks/datas";
+
+import type { FirewallInterface, FirewallInterfacesSummary } from "@homarr/integrations";
 
 dayjs.extend(duration);
 
@@ -177,4 +174,219 @@ export default function FirewallWidget({ integrationIds, width }: WidgetComponen
       </Accordion>
     </ScrollArea>
   );
+}
+
+
+export const useUpdatingCpuStatus = (integrationIds: string[]) => {
+  const utils = clientApi.useUtils();
+  const [firewallsCpuData] = clientApi.widget.firewall.getFirewallCpuStatus.useSuspenseQuery(
+    {
+      integrationIds,
+    },
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+    },
+  );
+
+  clientApi.widget.firewall.subscribeFirewallCpuStatus.useSubscription(
+    {
+      integrationIds,
+    },
+    {
+      onData: (data) => {
+        utils.widget.firewall.getFirewallCpuStatus.setData(
+          {
+            integrationIds,
+          },
+          (prevData) => {
+            if (!prevData) {
+              return undefined;
+            }
+
+            return prevData.map((item) =>
+              item.integration.id === data.integration.id ? { ...item, summary: data.summary } : item,
+            );
+          },
+        );
+      },
+    },
+  );
+
+  return firewallsCpuData;
+};
+
+export const useUpdatingMemoryStatus = (integrationIds: string[]) => {
+  const utils = clientApi.useUtils();
+  const [firewallsMemoryData] = clientApi.widget.firewall.getFirewallMemoryStatus.useSuspenseQuery(
+    {
+      integrationIds,
+    },
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+    },
+  );
+
+  clientApi.widget.firewall.subscribeFirewallMemoryStatus.useSubscription(
+    {
+      integrationIds,
+    },
+    {
+      onData: (data) => {
+        utils.widget.firewall.getFirewallMemoryStatus.setData(
+          {
+            integrationIds,
+          },
+          (prevData) => {
+            if (!prevData) {
+              return undefined;
+            }
+
+            return prevData.map((item) =>
+              item.integration.id === data.integration.id ? { ...item, summary: data.summary } : item,
+            );
+          },
+        );
+      },
+    },
+  );
+
+  return firewallsMemoryData;
+};
+
+export const useUpdatingVersionStatus = (integrationIds: string[]) => {
+  const utils = clientApi.useUtils();
+  const [firewallsVersionData] = clientApi.widget.firewall.getFirewallVersionStatus.useSuspenseQuery(
+    {
+      integrationIds,
+    },
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+    },
+  );
+
+  clientApi.widget.firewall.subscribeFirewallVersionStatus.useSubscription(
+    {
+      integrationIds,
+    },
+    {
+      onData: (data) => {
+        utils.widget.firewall.getFirewallVersionStatus.setData(
+          {
+            integrationIds,
+          },
+          (prevData) => {
+            if (!prevData) {
+              return undefined;
+            }
+
+            return prevData.map((item) =>
+              item.integration.id === data.integration.id ? { ...item, summary: data.summary } : item,
+            );
+          },
+        );
+      },
+    },
+  );
+  return firewallsVersionData;
+};
+
+export const useUpdatingInterfacesStatus = (integrationIds: string[]) => {
+  const utils = clientApi.useUtils();
+  const [firewallsInterfacesData] = clientApi.widget.firewall.getFirewallInterfacesStatus.useSuspenseQuery(
+    {
+      integrationIds,
+    },
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+    },
+  );
+
+  clientApi.widget.firewall.subscribeFirewallInterfacesStatus.useSubscription(
+    {
+      integrationIds,
+    },
+    {
+      onData: (data) => {
+        utils.widget.firewall.getFirewallInterfacesStatus.setData(
+          {
+            integrationIds,
+          },
+          (prevData) => {
+            if (!prevData) {
+              return undefined;
+            }
+            return prevData.map((item) =>
+              item.integration.id === data.integration.id ? { ...item, summary: data.summary } : item,
+            );
+          },
+        );
+      },
+    },
+  );
+
+  return firewallsInterfacesData;
+};
+
+export function formatBitsPerSec(bytes: number, decimals: number): string {
+  if (bytes === 0) return "0 Bytes";
+
+  const kilobyte = 1024;
+  const sizes = ["b/s", "kb/s", "Mb/s", "Gb/s", "Tb/s", "Pb/s", "Eb/s", "Zb/s", "Yb/s"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(kilobyte));
+
+  return `${parseFloat((bytes / Math.pow(kilobyte, i)).toFixed(decimals))} ${sizes[i]}`;
+}
+
+export function calculateBandwidth(data: FirewallInterfacesSummary[]): { data: FirewallInterface[] } {
+  const result = {
+    data: [] as FirewallInterface[],
+    timestamp: new Date().toISOString(),
+  };
+
+  if (data.length > 1) {
+    const firstData = data[0];
+    const secondData = data[1];
+
+    if (firstData && secondData) {
+      const time1 = new Date(firstData.timestamp);
+      const time2 = new Date(secondData.timestamp);
+      const timeDiffInSeconds = (time1.getTime() - time2.getTime()) / 1000;
+
+      firstData.data.forEach((iface) => {
+        const ifaceName = iface.name;
+        const recv1 = iface.receive;
+        const trans1 = iface.transmit;
+
+        const iface2 = secondData.data.find((i) => i.name === ifaceName);
+
+        if (iface2) {
+          const recv2 = iface2.receive;
+          const trans2 = iface2.transmit;
+          const recvDiff = recv1 - recv2;
+          const transDiff = trans1 - trans2;
+
+          result.data.push({
+            name: ifaceName,
+            receive: (8 * recvDiff) / timeDiffInSeconds,
+            transmit: (8 * transDiff) / timeDiffInSeconds,
+          });
+        }
+      });
+    }
+  }
+
+  return result;
 }
