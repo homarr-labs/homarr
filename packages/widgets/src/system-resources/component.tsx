3e1494a0cfff4a3d28298178155f07257ce782dd
Manuel <manuel.ruwe@protonmail.com>
2025-07-06 16:52:40 +0200

    feat: add graphs

--- packages/widgets/src/system-resources/component.tsx
+++ packages/widgets/src/system-resources/component.tsx
@@ -1,3 +1,18 @@
-import {WidgetComponentProps} from "../definition";
+import type {WidgetComponentProps} from "../definition";
+import {SystemResourceCPUChart} from "./chart/cpu-chart";
+import classes from "./component.module.css";
 
-export default function SystemResources({ options }: WidgetComponentProps<"systemResources">)
+export default function SystemResources({options}: WidgetComponentProps<"systemResources">) {
+  return (
+    <div className={classes.grid}>
+      <div className={classes.colSpanWide}>
+        <SystemResourceCPUChart/>
+      </div>
+      <div className={classes.colSpanWide}>
+        <SystemResourceCPUChart/>
+      </div>
+      <SystemResourceCPUChart/>
+      <SystemResourceCPUChart/>
+    </div>
+  )
+}
