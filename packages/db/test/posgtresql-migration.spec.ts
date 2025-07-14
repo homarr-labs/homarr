8dc1482fb5ff6da66ee4b6b3a6ffd2ee8097e07a
Yuichi Nakai <xoxyuxu@gmail.com>
2025-07-14 22:28:36 +0900

    feat: support postgresql database: add test

---
+++ packages/db/test/posgtresql-migration.spec.ts
@@ -0,0 +1,45 @@
+import path from "path";
+import { PostgreSqlContainer } from "@testcontainers/postgresql";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+import { describe, test } from "vitest";
+import { Pool } from "pg";
+import * as pgSchema from "../schema/postgresql";
+
+describe("PostgreSql Migration", () => {
+  test("should add all tables and keys specified in migration files", async () => {
+    const container = new PostgreSqlContainer("postgres:latest");
+    const postgreSqlContainer = await container.start();
+
+    const pool = new Pool({
+      user: postgreSqlContainer.getUsername(),
+      database: postgreSqlContainer.getDatabase(),
+      password: postgreSqlContainer.getPassword(),
+      port: postgreSqlContainer.getPort(),
+      host: postgreSqlContainer.getHost(),
+      keepAlive: true,
+      max: 0,
+      idleTimeoutMillis: 60000,
+      allowExitOnIdle: false,
+    });
+
+    const database = drizzle({
+      schema: pgSchema,
+      casing: "snake_case",
+      client: pool,
+    });
+
+    // Run migrations and check if it works
+    await migrate(database, {
+      migrationsFolder: path.join(__dirname, "..", "migrations", "postgresql"),
+    });
+
+    // Check if users table exists
+    await database.query.users.findMany();
+
+    // Close the pool to release resources
+    await pool.end();
+    // Stop the container
+    await postgreSqlContainer.stop();
+  }, 40_000);
+});
