279a2f134d08bffc7b9c32c661eca8fb7d07bd8f
Yuichi Nakai <xoxyuxu@gmail.com>
2025-07-14 22:28:36 +0900

    feat: support postgresql database: add test

--- packages/db/test/schema.spec.ts
+++ packages/db/test/schema.spec.ts
@@ -2,12 +2,15 @@
 import type { Column, InferSelectModel } from "drizzle-orm";
 import type { ForeignKey as MysqlForeignKey, MySqlTableWithColumns } from "drizzle-orm/mysql-core";
 import type { ForeignKey as SqliteForeignKey, SQLiteTableWithColumns } from "drizzle-orm/sqlite-core";
+import type { ForeignKey as PostgresqlForeignKey, PgTableWithColumns } from "drizzle-orm/pg-core";
 import { expect, expectTypeOf, test } from "vitest";
 
 import { objectEntries } from "@homarr/common";
 
 import * as mysqlSchema from "../schema/mysql";
 import * as sqliteSchema from "../schema/sqlite";
+import * as postgresqlSchema from "../schema/postgresql";
+// import { PostgreSqlContainer } from "@testcontainers/postgresql";
 
 // We need the following two types as there is currently no support for Buffer in mysql and
 // so we use a custom type which results in the config beeing different
@@ -18,6 +21,17 @@ type FixedMysqlConfig = {
     } & {
       dataType: MysqlConfig[key][column]["data"] extends Buffer ? "buffer" : MysqlConfig[key][column]["dataType"];
       data: MysqlConfig[key][column]["data"] extends Buffer ? Buffer : MysqlConfig[key][column]["data"];
+    };
+  };
+};
+
+type FixedPostgresqlConfig = {
+  [key in keyof PostgreisqlConfig]: {
+    [column in keyof PostgreisqlConfig[key]]: {
+      [property in Exclude<keyof PostgreisqlConfig[key][column], "dataType" | "data">]: PostgreisqlConfig[key][column][property];
+    } & {
+      dataType: PostgreisqlConfig[key][column]["data"] extends Buffer ? "buffer" : PostgreisqlConfig[key][column]["dataType"];
+      data: PostgreisqlConfig[key][column]["data"] extends Buffer ? Buffer : PostgreisqlConfig[key][column]["data"];
     };
   };
 };
@@ -38,6 +52,7 @@ test("schemas should match", () => {
   expectTypeOf<MysqlTables>().toEqualTypeOf<SqliteTables>();
   expectTypeOf<FixedSqliteConfig>().toEqualTypeOf<FixedMysqlConfig>();
   expectTypeOf<FixedMysqlConfig>().toEqualTypeOf<FixedSqliteConfig>();
+
 
   objectEntries(sqliteSchema).forEach(([tableName, sqliteTable]) => {
     Object.entries(sqliteTable).forEach(([columnName, sqliteColumn]: [string, object]) => {
@@ -117,6 +132,92 @@ test("schemas should match", () => {
   });
 });
 
+test("schemas should match for postgresql", () => {
+  expectTypeOf<SqliteTables>().toEqualTypeOf<PostgresqlTbale>();
+  expectTypeOf<PostgresqlTbale>().toEqualTypeOf<SqliteTables>();
+  expectTypeOf<FixedSqliteConfig>().toEqualTypeOf<FixedPostgresqlConfig>();
+  expectTypeOf<FixedPostgresqlConfig>().toEqualTypeOf<FixedSqliteConfig>();
+
+
+  objectEntries(sqliteSchema).forEach(([tableName, sqliteTable]) => {
+    Object.entries(sqliteTable).forEach(([columnName, sqliteColumn]: [string, object]) => {
+      if (!("isUnique" in sqliteColumn)) return;
+      if (!("uniqueName" in sqliteColumn)) return;
+      if (!("primary" in sqliteColumn)) return;
+
+      const postgresqlTable = postgresqlSchema[tableName];
+
+      const postgresqlColumn = postgresqlTable[columnName as keyof typeof postgresqlTable] as object;
+      if (!("isUnique" in postgresqlColumn)) return;
+      if (!("uniqueName" in postgresqlColumn)) return;
+      if (!("primary" in postgresqlColumn)) return;
+
+      expect(
+        sqliteColumn.isUnique,
+        `expect unique of column ${columnName} in table ${tableName} to be the same for both schemas`,
+      ).toEqual(postgresqlColumn.isUnique);
+      expect(
+        sqliteColumn.uniqueName,
+        `expect uniqueName of column ${columnName} in table ${tableName} to be the same for both schemas`,
+      ).toEqual(postgresqlColumn.uniqueName);
+      expect(
+        sqliteColumn.primary,
+        `expect primary of column ${columnName} in table ${tableName} to be the same for both schemas`,
+      ).toEqual(postgresqlColumn.primary);
+    });
+
+    const postgresqlTable = postgresqlSchema[tableName];
+    const sqliteForeignKeys = sqliteTable[Symbol.for("drizzle:SQLiteInlineForeignKeys") as keyof typeof sqliteTable] as
+      | SqliteForeignKey[]
+      | undefined;
+     const postgresqlForeignKeys = postgresqlTable[Symbol.for("drizzle:PgInlineForeignKeys") as keyof typeof postgresqlTable] as
+       | PostgresqlForeignKey[]
+       | undefined;
+    if (!sqliteForeignKeys && !postgresqlForeignKeys) return;
+
+    expect(postgresqlForeignKeys, `postgresql foreign key for ${tableName} to be defined`).toBeDefined();
+    expect(sqliteForeignKeys, `sqlite foreign key for ${tableName} to be defined`).toBeDefined();
+
+    expect(
+      sqliteForeignKeys!.length,
+      `expect number of foreign keys in table ${tableName} to be the same for both schemas`,
+    ).toEqual(postgresqlForeignKeys?.length);
+
+    sqliteForeignKeys?.forEach((sqliteForeignKey) => {
+      sqliteForeignKey.getName();
+      const postgresqlForeignKey = postgresqlForeignKeys!.find((key) => key.getName() === sqliteForeignKey.getName());
+      expect(
+        postgresqlForeignKey,
+        `expect foreign key ${sqliteForeignKey.getName()} to be defined in postgresql schema`,
+      ).toBeDefined();
+
+      // PostgreSql の場合、デフォルトで onUpdate は "no action" なので、Sqliteにあわせて undefined として扱う
+      expect(
+        sqliteForeignKey.onDelete,
+        `expect foreign key (${sqliteForeignKey.getName()}) onDelete to be the same for both schemas`,
+      ).toEqual(postgresqlForeignKey!.onDelete==='no action' ? undefined : postgresqlForeignKey!.onDelete);
+
+      // PostgreSql の場合、デフォルトで onUpdate は "no action" なので、Sqliteにあわせて undefined として扱う
+      expect(
+        sqliteForeignKey.onUpdate,
+        `expect foreign key (${sqliteForeignKey.getName()}) onUpdate to be the same for both schemas`,
+      ).toEqual(postgresqlForeignKey!.onUpdate==='no action' ? undefined : postgresqlForeignKey!.onUpdate);
+
+      sqliteForeignKey.reference().foreignColumns.forEach((column) => {
+        expect(
+          postgresqlForeignKey!.reference().foreignColumns.map((column) => column.name),
+          `expect foreign key (${sqliteForeignKey.getName()}) columns to be the same for both schemas`,
+        ).toContainEqual(column.name);
+      });
+
+      expect(
+        Object.keys(sqliteForeignKey.reference().foreignTable),
+        `expect foreign key (${sqliteForeignKey.getName()}) table to be the same for both schemas`,
+      ).toEqual(Object.keys(postgresqlForeignKey!.reference().foreignTable).filter((key) => key !== "enableRLS"));
+    });
+  });
+});
+
 type SqliteTables = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [K in keyof typeof sqliteSchema]: (typeof sqliteSchema)[K] extends SQLiteTableWithColumns<any>
@@ -127,6 +228,13 @@ type MysqlTables = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [K in keyof typeof mysqlSchema]: (typeof mysqlSchema)[K] extends MySqlTableWithColumns<any>
     ? InferSelectModel<(typeof mysqlSchema)[K]>
+    : never;
+};
+
+type PostgresqlTbale = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [K in keyof typeof postgresqlSchema]: (typeof postgresqlSchema)[K] extends PgTableWithColumns<any>
+    ? InferSelectModel<(typeof postgresqlSchema)[K]>
     : never;
 };
 
@@ -151,6 +259,17 @@ type MysqlConfig = {
     ? {
         [C in keyof (typeof mysqlSchema)[K]["_"]["config"]["columns"]]: InferColumnConfig<
           (typeof mysqlSchema)[K]["_"]["config"]["columns"][C]
+        >;
+      }
+    : never;
+};
+
+type PostgreisqlConfig = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [K in keyof typeof postgresqlSchema]: (typeof postgresqlSchema)[K] extends PgTableWithColumns<any>
+    ? {
+        [C in keyof (typeof postgresqlSchema)[K]["_"]["config"]["columns"]]: InferColumnConfig<
+          (typeof postgresqlSchema)[K]["_"]["config"]["columns"][C]
         >;
       }
     : never;
