c52ca9df385760c0e5a95303481d5efdd3f2c1e2
Aandree5 <andre-silva5@hotmail.com>
2025-07-14 23:01:44 +0100

    feat(releases-widget): added `Github Packages` integration

--- packages/integrations/src/github-packages/github-packages-integration.ts
+++ packages/integrations/src/github-packages/github-packages-integration.ts
@@ -8,7 +8,13 @@ import { Integration } from "../base/integration";
 import { TestConnectionError } from "../base/test-connection/test-connection-error";
 import type { TestingResult } from "../base/test-connection/test-connection-service";
 import type { ReleasesProviderIntegration } from "../interfaces/releases-providers/releases-providers-integration";
-import type { ReleasesRepository, ReleasesResponse } from "../interfaces/releases-providers/releases-providers-types";
+import { getLatestRelease } from "../interfaces/releases-providers/releases-providers-integration";
+import type {
+  DetailsProviderResponse,
+  ReleaseProviderResponse,
+  ReleasesRepository,
+  ReleasesResponse,
+} from "../interfaces/releases-providers/releases-providers-types";
 
 const localLogger = logger.child({ module: "GithubPackagesIntegration" });
 
@@ -52,37 +58,31 @@ export class GithubPackagesIntegration extends Integration implements ReleasesPr
     }
 
     const api = this.getApi();
+    const details = await this.getDetailsAsync(api, owner, name);
 
     try {
-      const releasesResponse = await api.rest.packages.getPackageForUser({
+      const versionResponse = await api.rest.packages.getAllPackageVersionsForPackageOwnedByUser({
         username: owner,
         package_type: "container",
         package_name: name,
+        per_page: 100,
       });
 
-      // List package versions for a package owned by a user
-      // missing from octokit ??
-      const versionResponse = await api.rest.packages.packages({
-        username: owner,
-        package_type: "container",
-        package_name: name,
-      });
+      const releasesProviderResponse = versionResponse.data.reduce<ReleaseProviderResponse[]>((acc, release) => {
+        if (!release.metadata?.container?.tags || !(release.metadata.container.tags.length > 0)) return acc;
 
-      return {
-        id: repository.id,
-        projectUrl: releasesResponse.data.repository?.html_url ?? releasesResponse.data.html_url,
-        projectDescription: releasesResponse.data.repository?.description ?? undefined,
-        isFork: releasesResponse.data.repository?.fork,
-        isArchived: releasesResponse.data.repository?.archived,
-        createdAt: new Date(releasesResponse.data.created_at),
-        starsCount: releasesResponse.data.repository?.stargazers_count,
-        openIssues: releasesResponse.data.repository?.open_issues_count,
-        forksCount: releasesResponse.data.repository?.forks_count,
-        latestRelease: versionResponse.data.name,
-        latestReleaseAt: new Date(versionResponse.data.updated_at),
-        releaseUrl: versionResponse.data.html_url,
-        releaseDescription: versionResponse.data.description ?? undefined,
-      };
+        release.metadata.container.tags.forEach((tag) => {
+          acc.push({
+            latestRelease: tag,
+            latestReleaseAt: new Date(release.updated_at),
+            releaseUrl: release.html_url,
+            releaseDescription: release.description ?? undefined,
+          });
+        });
+        return acc;
+      }, []);
+
+      return getLatestRelease(releasesProviderResponse, repository, details);
     } catch (error) {
       const errorMessage = error instanceof RequestError ? error.message : String(error);
 
@@ -96,6 +96,38 @@ export class GithubPackagesIntegration extends Integration implements ReleasesPr
         id: repository.id,
         error: { message: errorMessage },
       };
+    }
+  }
+
+  protected async getDetailsAsync(
+    api: Octokit,
+    owner: string,
+    name: string,
+  ): Promise<DetailsProviderResponse | undefined> {
+    try {
+      const response = await api.rest.packages.getPackageForUser({
+        username: owner,
+        package_type: "container",
+        package_name: name,
+      });
+
+      return {
+        projectUrl: response.data.repository?.html_url ?? response.data.html_url,
+        projectDescription: response.data.repository?.description ?? undefined,
+        isFork: response.data.repository?.fork,
+        isArchived: response.data.repository?.archived,
+        createdAt: new Date(response.data.created_at),
+        starsCount: response.data.repository?.stargazers_count,
+        openIssues: response.data.repository?.open_issues_count,
+        forksCount: response.data.repository?.forks_count,
+      };
+    } catch (error) {
+      localLogger.warn(`Failed to get details for ${owner}\\${name} with Github Packages integration`, {
+        owner,
+        name,
+        error: error instanceof RequestError ? error.message : String(error),
+      });
+      return undefined;
     }
   }
 
