c4880dff5123534d576a65ffb21e9166d47eea02
Meier Lukas <meierschlumpf@gmail.com>
2025-07-16 16:00:43 +0200

    wip: add external redis

---
+++ packages/core/src/infrastructure/redis/client.ts
@@ -0,0 +1,26 @@
+import type { RedisOptions } from "ioredis";
+import { Redis } from "ioredis";
+
+import { redisEnv } from "./env";
+
+const defaultRedisOptions = {
+  connectionName: "homarr",
+} satisfies RedisOptions;
+
+export type { Redis as RedisClient } from "ioredis";
+
+export const createRedisClient = () =>
+  redisEnv.IS_EXTERNAL
+    ? new Redis({
+        ...defaultRedisOptions,
+        host: redisEnv.HOST,
+        port: redisEnv.PORT,
+        tls: redisEnv.TLS_CA
+          ? {
+              ca: redisEnv.TLS_CA,
+            }
+          : undefined,
+        username: redisEnv.USERNAME,
+        password: redisEnv.PASSWORD,
+      })
+    : new Redis(defaultRedisOptions);
