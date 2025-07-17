c4e5c3002bd1c746cee496b5ba6d19159104b7f8
Meier Lukas <meierschlumpf@gmail.com>
2025-07-17 10:43:13 +0200

    feat(logs): add log level selection to tools ui (#3565)

--- packages/api/src/router/log.ts
+++ packages/api/src/router/log.ts
@@ -1,22 +1,33 @@
 import { observable } from "@trpc/server/observable";
+import z from "zod";
 
 import { logger } from "@homarr/log";
+import { logLevels } from "@homarr/log/constants";
 import type { LoggerMessage } from "@homarr/redis";
 import { loggingChannel } from "@homarr/redis";
+import { zodEnumFromArray } from "@homarr/validation/enums";
 
 import { createTRPCRouter, permissionRequiredProcedure } from "../trpc";
 
 export const logRouter = createTRPCRouter({
-  subscribe: permissionRequiredProcedure.requiresPermission("other-view-logs").subscription(() => {
-    return observable<LoggerMessage>((emit) => {
-      const unsubscribe = loggingChannel.subscribe((data) => {
-        emit.next(data);
-      });
-      logger.info("A tRPC client has connected to the logging procedure");
+  subscribe: permissionRequiredProcedure
+    .requiresPermission("other-view-logs")
+    .input(
+      z.object({
+        levels: z.array(zodEnumFromArray(logLevels)).default(["info"]),
+      }),
+    )
+    .subscription(({ input }) => {
+      return observable<LoggerMessage>((emit) => {
+        const unsubscribe = loggingChannel.subscribe((data) => {
+          if (!input.levels.includes(data.level)) return;
+          emit.next(data);
+        });
+        logger.info("A tRPC client has connected to the logging procedure");
 
-      return () => {
-        unsubscribe();
-      };
-    });
-  }),
+        return () => {
+          unsubscribe();
+        };
+      });
+    }),
 });
