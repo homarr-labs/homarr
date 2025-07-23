c8d160c612f2d683583b838631dbfdb8bb5a133b
Manuel <manuel.ruwe@protonmail.com>
2025-07-23 20:52:15 +0200

    revert: "feat(ping): ignore certificate error and show request duration (#3546)"
    This reverts commit 1eb47311fa81184bd67830d0669d9f87d9922e70.

---
+++ packages/ping/src/index.ts
@@ -0,0 +1,34 @@
+import type { fetch } from "undici";
+
+import { fetchWithTrustedCertificatesAsync } from "@homarr/certificates/server";
+import { extractErrorMessage } from "@homarr/common";
+import { logger } from "@homarr/log";
+
+export const sendPingRequestAsync = async (url: string) => {
+  try {
+    return await fetchWithTimeoutAndCertificates(url).then((response) => ({ statusCode: response.status }));
+  } catch (error) {
+    logger.error(new Error(`Failed to send ping request to "${url}"`, { cause: error }));
+    return {
+      error: extractErrorMessage(error),
+    };
+  }
+};
+
+/**
+ * Same as fetch, but with a timeout of 10 seconds.
+ * Also respects certificates.
+ * https://stackoverflow.com/questions/46946380/fetch-api-request-timeout
+ * @param param0 fetch arguments
+ * @returns fetch response
+ */
+export const fetchWithTimeoutAndCertificates = (...[url, requestInit]: Parameters<typeof fetch>) => {
+  const controller = new AbortController();
+
+  // 10 seconds timeout:
+  const timeoutId = setTimeout(() => controller.abort(), 10000);
+
+  return fetchWithTrustedCertificatesAsync(url, { signal: controller.signal, ...requestInit }).finally(() => {
+    clearTimeout(timeoutId);
+  });
+};
--- packages/widgets/src/app/ping/ping-indicator.tsx
+++ packages/widgets/src/app/ping/ping-indicator.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import { IconCheck, IconLoader, IconX } from "@tabler/icons-react";
+import { IconCheck, IconX } from "@tabler/icons-react";
 
 import type { RouterOutputs } from "@homarr/api";
 import { clientApi } from "@homarr/api/client";
-import { useI18n } from "@homarr/translation/client";
 
 import { PingDot } from "./ping-dot";
 
@@ -12,8 +11,17 @@ interface PingIndicatorProps {
 }
 
 export const PingIndicator = ({ href }: PingIndicatorProps) => {
-  const t = useI18n();
-  const [pingResult, setPingResult] = useState<RouterOutputs["widget"]["app"]["updatedPing"] | null>(null);
+  const [ping] = clientApi.widget.app.ping.useSuspenseQuery(
+    {
+      url: href,
+    },
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  const [pingResult, setPingResult] = useState<RouterOutputs["widget"]["app"]["ping"]>(ping);
 
   clientApi.widget.app.updatedPing.useSubscription(
     { url: href },
@@ -24,21 +32,13 @@ export const PingIndicator = ({ href }: PingIndicatorProps) => {
     },
   );
 
-  if (!pingResult) {
-    return <PingDot icon={IconLoader} color="blue" tooltip={`${t("common.action.loading")}…`} />;
-  }
-
   const isError = "error" in pingResult || pingResult.statusCode >= 500;
 
   return (
     <PingDot
       icon={isError ? IconX : IconCheck}
       color={isError ? "red" : "green"}
-      tooltip={
-        "statusCode" in pingResult
-          ? `${pingResult.statusCode} - ${pingResult.durationMs.toFixed(0)}ms`
-          : pingResult.error
-      }
+      tooltip={"statusCode" in pingResult ? pingResult.statusCode.toString() : pingResult.error}
     />
   );
 };
