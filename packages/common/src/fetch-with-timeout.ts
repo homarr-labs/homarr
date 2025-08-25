884f063b36d3aba8fc30ede4dc238112cbe49ea9
Meier Lukas <meierschlumpf@gmail.com>
2025-08-25 22:47:36 +0200

    fix: fetch-with-timeout is exported to client

--- packages/common/src/fetch-with-timeout.ts
+++ packages/common/src/fetch-with-timeout.ts
@@ -1,6 +1,3 @@
-// The below import statement initializes dns-caching
-import "./dns";
-
 /**
  * Same as fetch, but with a timeout of 10 seconds.
  * https://stackoverflow.com/questions/46946380/fetch-api-request-timeout
