4ae6594c92f6cfb76823a658e24f8862c7e31fbb
Meier Lukas <meierschlumpf@gmail.com>
2025-08-28 22:52:25 +0200

    fix: add documentation-link for truenas integration

--- packages/definitions/src/integration.ts
+++ packages/definitions/src/integration.ts
@@ -285,6 +285,7 @@ export const integrationDefs = {
     secretKinds: [["username", "password"]],
     iconUrl: "https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons@master/svg/truenas.svg",
     category: ["healthMonitoring"],
+    documentationUrl: createDocumentationLink("/docs/integrations/truenas"),
   },
   // This integration only returns mock data, it is used during development (but can also be used in production by directly going to the create page)
   mock: {
