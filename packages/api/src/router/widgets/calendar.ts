2443d7300b958edabceb95ab8663b9bebb30e4d0
Meier Lukas <meierschlumpf@gmail.com>
2025-09-26 20:03:34 +0200

    feat(calendar): add periodic live updates (#4154)

--- packages/api/src/router/widgets/calendar.ts
+++ packages/api/src/router/widgets/calendar.ts
@@ -1,6 +1,11 @@
+import { observable } from "@trpc/server/observable";
 import { z } from "zod/v4";
 
+import type { Modify } from "@homarr/common/types";
+import type { Integration } from "@homarr/db/schema";
+import type { IntegrationKindByCategory } from "@homarr/definitions";
 import { getIntegrationKindsByCategory } from "@homarr/definitions";
+import type { CalendarEvent } from "@homarr/integrations/types";
 import { radarrReleaseTypes } from "@homarr/integrations/types";
 import { calendarMonthRequestHandler } from "@homarr/request-handler/calendar";
 
@@ -19,14 +24,56 @@ export const calendarRouter = createTRPCRouter({
     )
     .concat(createManyIntegrationMiddleware("query", ...getIntegrationKindsByCategory("calendar")))
     .query(async ({ ctx, input }) => {
-      const results = await Promise.all(
+      return await Promise.all(
         ctx.integrations.map(async (integration) => {
-          const innerHandler = calendarMonthRequestHandler.handler(integration, input);
+          const { integrationIds: _integrationIds, ...handlerInput } = input;
+          const innerHandler = calendarMonthRequestHandler.handler(integration, handlerInput);
           const { data } = await innerHandler.getCachedOrUpdatedDataAsync({ forceUpdate: false });
 
-          return data;
+          return {
+            events: data,
+            integration: {
+              id: integration.id,
+              name: integration.name,
+              kind: integration.kind,
+            },
+          };
         }),
       );
-      return results.flat();
+    }),
+  subscribeToEvents: publicProcedure
+    .input(
+      z.object({
+        year: z.number(),
+        month: z.number(),
+        releaseType: z.array(z.enum(radarrReleaseTypes)),
+        showUnmonitored: z.boolean(),
+      }),
+    )
+    .concat(createManyIntegrationMiddleware("query", ...getIntegrationKindsByCategory("calendar")))
+    .subscription(({ ctx, input }) => {
+      return observable<{
+        integration: Modify<Integration, { kind: IntegrationKindByCategory<"calendar"> }>;
+        events: CalendarEvent[];
+      }>((emit) => {
+        const unsubscribes: (() => void)[] = [];
+        for (const integrationWithSecrets of ctx.integrations) {
+          const { decryptedSecrets: _, ...integration } = integrationWithSecrets;
+          const { integrationIds: _integrationIds, ...handlerInput } = input;
+          const innerHandler = calendarMonthRequestHandler.handler(integrationWithSecrets, handlerInput);
+          const unsubscribe = innerHandler.subscribe((events) => {
+            emit.next({
+              integration,
+              events,
+            });
+          });
+          unsubscribes.push(unsubscribe);
+        }
+        return () => {
+          unsubscribes.forEach((unsubscribe) => {
+            unsubscribe();
+          });
+        };
+      });
     }),
 });
