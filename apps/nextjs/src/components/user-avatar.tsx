61a7b5c0fa670b7a3e612010d7f65be5890da3b6
HeapReaper <kelivn@heapreaper.nl>
2025-10-12 16:39:14 +0200

    Added gravatar support

--- apps/nextjs/src/components/user-avatar.tsx
+++ apps/nextjs/src/components/user-avatar.tsx
@@ -1,19 +1,22 @@
 import type { MantineSize } from "@mantine/core";
 
 import { auth } from "@homarr/auth/next";
-import { UserAvatar } from "@homarr/ui";
+import { UserAvatar, UserProps } from "@homarr/ui";
 
-interface UserAvatarProps {
+interface CurrentUserAvatarProps {
   size: MantineSize;
 }
 
-export const CurrentUserAvatar = async ({ size }: UserAvatarProps) => {
+export const CurrentUserAvatar = async ({ size }: CurrentUserAvatarProps) => {
   const currentSession = await auth();
 
-  const user = {
+  const user: UserProps = {
     name: currentSession?.user.name ?? null,
     image: currentSession?.user.image ?? null,
+    email: currentSession?.user.email ?? null,
   };
+
+  if (!user.image && !user.email && user.name) user.image = null;
 
   return <UserAvatar user={user} size={size} />;
 };
