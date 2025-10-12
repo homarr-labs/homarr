afbc6422e5d3a65273ba31247a151b76d1fbab34
HeapReaper <kelivn@heapreaper.nl>
2025-10-12 16:39:14 +0200

    feat: add gravatar support

--- packages/ui/src/components/user-avatar.tsx
+++ packages/ui/src/components/user-avatar.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import type { AvatarProps } from "@mantine/core";
 import { Avatar } from "@mantine/core";
+import { enc, MD5 } from "crypto-js";
 
 export interface UserProps {
   name: string | null;
   image: string | null;
+  email?: string | null;
 }
 
 interface UserAvatarProps {
@@ -13,8 +17,14 @@ interface UserAvatarProps {
 
 export const UserAvatar = ({ user, size }: UserAvatarProps) => {
   if (!user?.name) return <Avatar size={size} />;
+
   if (user.image) {
     return <Avatar src={user.image} alt={user.name} size={size} />;
+  }
+
+  if (user.email) {
+    const emailHash = MD5(user.email.trim().toLowerCase()).toString(enc.Hex);
+    return <Avatar src={`https://seccdn.libravatar.org/avatar/${emailHash}?d=blank`} alt={user.name} size={size} />;
   }
 
   return <Avatar name={user.name} color="initials" size={size}></Avatar>;
