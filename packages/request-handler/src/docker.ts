d564ef1b9819944ab6b239faddbb8bbf364374ba
Meier Lukas <meierschlumpf@gmail.com>
2025-10-18 18:20:03 +0200

    fix: cpu usage always zero

--- packages/request-handler/src/docker.ts
+++ packages/request-handler/src/docker.ts
@@ -73,11 +73,10 @@ async function getContainersWithStatsAsync() {
 }
 
 function calculateCpuUsage(stats: ContainerStats): number {
-  const cpuDelta = stats.cpu_stats.cpu_usage.total_usage - stats.precpu_stats.cpu_usage.total_usage;
-  const systemDelta = stats.cpu_stats.system_cpu_usage - stats.precpu_stats.system_cpu_usage;
   const numberOfCpus = stats.cpu_stats.online_cpus || 1;
 
-  if (systemDelta === 0) return 0;
+  const usage = stats.cpu_stats.system_cpu_usage;
+  if (usage === 0) return 0;
 
-  return (cpuDelta / systemDelta) * numberOfCpus * 100;
+  return (stats.cpu_stats.cpu_usage.total_usage / usage) * numberOfCpus * 100;
 }
