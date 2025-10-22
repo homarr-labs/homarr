9773ce9c70466e239f5ea173c456687021746dea
Meier Lukas <meierschlumpf@gmail.com>
2025-10-22 21:05:08 +0200

    fix: typecheck issue

--- packages/db/migrations/seed.ts
+++ packages/db/migrations/seed.ts
@@ -150,6 +150,7 @@ const seedDefaultIntegrationsAsync = async (db: Database) => {
         name: `${name} Default`,
         url: defaultUrl,
         kind,
+        appId: null,
       });
     }
 
