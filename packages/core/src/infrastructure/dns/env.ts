483054cb08a6070d1948753a959460ad8c4e515f
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-12-19 21:45:24 +0100

    feat: enable dns caching by default

--- packages/core/src/infrastructure/dns/env.ts
+++ packages/core/src/infrastructure/dns/env.ts
@@ -2,7 +2,7 @@ import { createBooleanSchema, createEnv } from "../env";
 
 export const dnsEnv = createEnv({
   server: {
-    ENABLE_DNS_CACHING: createBooleanSchema(false),
+    ENABLE_DNS_CACHING: createBooleanSchema(true),
   },
   experimental__runtimeEnv: process.env,
 });
