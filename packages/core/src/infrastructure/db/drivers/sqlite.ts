9a8e1dcae95e6f2dd67100d1f43795a98df475dd
Meier Lukas <meierschlumpf@gmail.com>
2026-01-04 14:44:41 +0100

    perf: migrate to libsql

--- packages/core/src/infrastructure/db/drivers/sqlite.ts
+++ packages/core/src/infrastructure/db/drivers/sqlite.ts
@@ -1,12 +1,12 @@
-import type BetterSqlite3Db from "better-sqlite3";
-import { drizzle as drizzleSqlite } from "drizzle-orm/better-sqlite3";
+import { createClient } from "@libsql/client/node";
+import { drizzle as drizzleSqlite } from "drizzle-orm/libsql";
 
 import { dbEnv } from "../env";
 import type { SharedDrizzleConfig } from "./shared";
 
 export const createSqliteDb = <TSchema extends Record<string, unknown>>(config: SharedDrizzleConfig<TSchema>) => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports
-  const Database: typeof BetterSqlite3Db = require("better-sqlite3");
-  const connection = new Database(dbEnv.URL);
+  const connection = createClient({
+    url: "file://" + dbEnv.URL,
+  });
   return drizzleSqlite<TSchema>(connection, config);
 };
--- packages/db/collection.ts
+++ packages/db/collection.ts
@@ -1,22 +1,13 @@
 import type { InferInsertModel } from "drizzle-orm";
 
 import { objectEntries } from "@homarr/common";
-import { dbEnv } from "@homarr/core/infrastructure/db/env";
 
-import type { HomarrDatabase, HomarrDatabaseMysql, HomarrDatabasePostgresql } from "./driver";
+import type { HomarrDatabase, HomarrTransaction } from "./driver";
 import * as schema from "./schema";
 
 type TableKey = {
   [K in keyof typeof schema]: (typeof schema)[K] extends { _: { brand: "Table" } } ? K : never;
 }[keyof typeof schema];
-
-export function isMysql(): boolean {
-  return dbEnv.DRIVER === "mysql2";
-}
-
-export function isPostgresql(): boolean {
-  return dbEnv.DRIVER === "node-postgres";
-}
 
 export const createDbInsertCollectionForTransaction = <TTableKey extends TableKey>(
   tablesInInsertOrder: TTableKey[],
@@ -31,22 +22,8 @@ export const createDbInsertCollectionForTransaction = <TTableKey extends TableKe
 
   return {
     ...context,
-    insertAll: (db: HomarrDatabase) => {
-      db.transaction((transaction) => {
-        for (const [key, values] of objectEntries(context)) {
-          if (values.length >= 1) {
-            transaction
-              .insert(schema[key])
-              .values(values as never)
-              .run();
-          }
-        }
-      });
-    },
-    // We allow any database that supports async passed here but then fallback to mysql to prevent typescript errors
-    insertAllAsync: async (db: HomarrDatabaseMysql | HomarrDatabasePostgresql) => {
-      const innerDb = db as HomarrDatabaseMysql;
-      await innerDb.transaction(async (transaction) => {
+    insertAllAsync: async (db: HomarrDatabase | HomarrTransaction) => {
+      await db.transaction(async (transaction) => {
         for (const [key, values] of objectEntries(context)) {
           if (values.length >= 1) {
             // Below is actually the mysqlSchema when the driver is mysql
@@ -61,23 +38,12 @@ export const createDbInsertCollectionForTransaction = <TTableKey extends TableKe
 export const createDbInsertCollectionWithoutTransaction = <TTableKey extends TableKey>(
   tablesInInsertOrder: TTableKey[],
 ) => {
-  const { insertAll, insertAllAsync, ...collection } = createDbInsertCollectionForTransaction(tablesInInsertOrder);
+  const { insertAllAsync, ...collection } = createDbInsertCollectionForTransaction(tablesInInsertOrder);
 
   return {
     ...collection,
     insertAllAsync: async (db: HomarrDatabase) => {
-      switch (dbEnv.DRIVER) {
-        case "mysql2":
-        case "node-postgres":
-          // For mysql2 and node-postgres, we can use the async insertAllAsync method
-          await insertAllAsync(db as unknown as HomarrDatabaseMysql | HomarrDatabasePostgresql);
-          return;
-        default:
-          // For better-sqlite3, we need to use the synchronous insertAll method
-          // default assumes better-sqlite3. It's original implementation.
-          insertAll(db);
-          break;
-      }
+      await insertAllAsync(db);
     },
   };
 };
