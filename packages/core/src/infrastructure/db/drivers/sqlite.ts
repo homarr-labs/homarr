1fa4361700cd8dec20e220c54cac35d1933f00c1
Meier Lukas <meierschlumpf@gmail.com>
2026-01-04 13:58:13 +0100

    perf: only create sqlite-db if necessary

--- packages/core/src/infrastructure/db/drivers/sqlite.ts
+++ packages/core/src/infrastructure/db/drivers/sqlite.ts
@@ -1,10 +1,12 @@
-import Database from "better-sqlite3";
+import type BetterSqlite3Db from "better-sqlite3";
 import { drizzle as drizzleSqlite } from "drizzle-orm/better-sqlite3";
 
 import { dbEnv } from "../env";
 import type { SharedDrizzleConfig } from "./shared";
 
 export const createSqliteDb = <TSchema extends Record<string, unknown>>(config: SharedDrizzleConfig<TSchema>) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports
+  const Database: typeof BetterSqlite3Db = require("better-sqlite3");
   const connection = new Database(dbEnv.URL);
   return drizzleSqlite<TSchema>(connection, config);
 };
