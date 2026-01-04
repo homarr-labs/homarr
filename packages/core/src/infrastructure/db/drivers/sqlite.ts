031a5d922a4814d2fc115851771ca927908154bd
Meier Lukas <meierschlumpf@gmail.com>
2026-01-04 15:00:14 +0100

    fix: connection string not working

--- packages/core/src/infrastructure/db/drivers/sqlite.ts
+++ packages/core/src/infrastructure/db/drivers/sqlite.ts
@@ -6,7 +6,7 @@ import type { SharedDrizzleConfig } from "./shared";
 
 export const createSqliteDb = <TSchema extends Record<string, unknown>>(config: SharedDrizzleConfig<TSchema>) => {
   const connection = createClient({
-    url: "file://" + dbEnv.URL,
+    url: "file:" + (dbEnv.URL || "/appdata/db/db.sqlite"),
   });
   return drizzleSqlite<TSchema>(connection, config);
 };
