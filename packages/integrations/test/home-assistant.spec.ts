b39427e43bbfc7c8d89490cf69806e0cf4d5e613
Omar <omariqbal97@gmail.com>
2026-03-11 21:53:05 -0400

    revert: reverting changes in  Home Assistant integration test

--- packages/integrations/test/home-assistant.spec.ts
+++ packages/integrations/test/home-assistant.spec.ts
@@ -1,11 +1,35 @@
+import { join } from "path";
 import type { StartedTestContainer } from "testcontainers";
 import { GenericContainer, getContainerRuntimeClient, ImageName, Wait } from "testcontainers";
-import { beforeAll, describe, expect, test } from "vitest";
+import { beforeAll, describe, expect, test, vi } from "vitest";
 
-import { HomeAssistantIntegration, IntegrationTestConnectionError } from "../src";
+import { createDb } from "@homarr/db/test";
+
+import { HomeAssistantIntegration } from "../src";
+import { TestConnectionError } from "../src/base/test-connection/test-connection-error";
+
+vi.mock("@homarr/db", async (importActual) => {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-imports
+  const actual = await importActual<typeof import("@homarr/db")>();
+  return {
+    ...actual,
+    db: createDb(),
+  };
+});
+
+vi.mock("@homarr/core/infrastructure/certificates", async (importActual) => {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-imports
+  const actual = await importActual<typeof import("@homarr/core/infrastructure/certificates")>();
+  return {
+    ...actual,
+    getTrustedCertificateHostnamesAsync: vi.fn().mockImplementation(() => {
+      return Promise.resolve([]);
+    }),
+  };
+});
 
 const DEFAULT_API_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkNjQwY2VjNDFjOGU0NGM5YmRlNWQ4ZmFjMjUzYWViZiIsImlhdCI6MTcxODQ3MTE1MSwiZXhwIjoyMDMzODMxMTUxfQ.uQCZ5FZTokipa6N27DtFhLHkwYEXU1LZr0fsVTryL2Q";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkNjQwY2VjNDFjOGU0NGM5YmRlNWQ4ZmFjMjUzYWViZiIsImlhdCI6MTcxODQ3MTE1MSwiZXhwIjoyMDMzODMxMTUxfQ.uQCZ5FZTokipa6N27DtFhLHkwYEXU1LZr0fsVTryL2Q";
 const IMAGE_NAME = "ghcr.io/home-assistant/home-assistant:stable";
 
 describe("Home Assistant integration", () => {
@@ -20,53 +44,60 @@ describe("Home Assistant integration", () => {
     const homeAssistantIntegration = createHomeAssistantIntegration(startedContainer);
 
     // Act
-    const actAsync = async () => await homeAssistantIntegration.testConnectionAsync();
+    const result = await homeAssistantIntegration.testConnectionAsync();
 
     // Assert
-    await expect(actAsync()).resolves.not.toThrow();
+    expect(result.success).toBe(true);
 
     // Cleanup
     await startedContainer.stop();
-  }, 20_000); // Timeout of 20 seconds
+  }, 30_000); // Timeout of 30 seconds
   test("Test connection should fail with wrong credentials", async () => {
     // Arrange
     const startedContainer = await prepareHomeAssistantContainerAsync();
     const homeAssistantIntegration = createHomeAssistantIntegration(startedContainer, "wrong-api-key");
 
     // Act
-    const actAsync = async () => await homeAssistantIntegration.testConnectionAsync();
+    const result = await homeAssistantIntegration.testConnectionAsync();
 
     // Assert
-    await expect(actAsync()).rejects.toThrow(IntegrationTestConnectionError);
+    expect(result.success).toBe(false);
+    if (result.success) return;
+
+    expect(result.error).toBeInstanceOf(TestConnectionError);
+    expect(result.error.type).toBe("authorization");
 
     // Cleanup
     await startedContainer.stop();
-  }, 20_000); // Timeout of 20 seconds
+  }, 30_000); // Timeout of 30 seconds
 });
 
 const prepareHomeAssistantContainerAsync = async () => {
   const homeAssistantContainer = createHomeAssistantContainer();
   const startedContainer = await homeAssistantContainer.start();
-
   await startedContainer.exec(["unzip", "-o", "/tmp/config.zip", "-d", "/config"]);
   await startedContainer.restart();
   return startedContainer;
 };
 
 const createHomeAssistantContainer = () => {
-  return new GenericContainer(IMAGE_NAME)
-    .withCopyFilesToContainer([
-      {
-        source: __dirname + "/volumes/home-assistant-config.zip",
-        target: "/tmp/config.zip",
-      },
-    ])
-    .withPrivilegedMode()
-    .withExposedPorts(8123)
-    .withWaitStrategy(Wait.forHttp("/", 8123));
+  return (
+    new GenericContainer(IMAGE_NAME)
+      .withCopyFilesToContainer([
+        {
+          source: join(__dirname, "/volumes/home-assistant-config.zip"),
+          target: "/tmp/config.zip",
+        },
+      ])
+      .withPrivilegedMode()
+      .withExposedPorts(8123)
+      // This has to be a page that is not redirected (or a status code has to be defined withStatusCode(statusCode))
+      .withWaitStrategy(Wait.forHttp("/onboarding.html", 8123))
+  );
 };
 
 const createHomeAssistantIntegration = (container: StartedTestContainer, apiKeyOverride?: string) => {
+  console.log("Creating Home Assistant integration...");
   return new HomeAssistantIntegration({
     id: "1",
     decryptedSecrets: [
@@ -77,5 +108,6 @@ const createHomeAssistantIntegration = (container: StartedTestContainer, apiKeyO
     ],
     name: "Home assistant",
     url: `http://${container.getHost()}:${container.getMappedPort(8123)}`,
+    externalUrl: null,
   });
 };
