51919724a397929013f4ffc12e5efd2f74667c80
Meier Lukas <meierschlumpf@gmail.com>
2026-04-13 17:42:58 +0200

    fix: support catalog entries for package-reader

--- apps/nextjs/src/versions/package-reader.ts
+++ apps/nextjs/src/versions/package-reader.ts
@@ -1,5 +1,9 @@
 import fsPromises from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 import { glob } from "glob";
+import { parse as parseYaml } from "yaml";
+import z from "zod";
 
 import packageJson from "../../../../package.json";
 
@@ -15,12 +19,35 @@ export const getDependenciesAsync = async (): Promise<PackageJsonDependencies> =
     .map((packageContent) => (JSON.parse(packageContent) as PackageJson).dependencies)
     .filter((dependencies) => dependencies !== undefined);
 
+  const catalog = await parseDependencyCatalogsAsync();
+
   let dependencies = {};
   for (const dependenciesOfPackage of packageDependencies) {
-    dependencies = { ...dependencies, ...dependenciesOfPackage };
+    const resolvedDependencies = Object.entries(dependenciesOfPackage).map(([name, version]) => {
+      const catalogVersion = catalog.get(name);
+      return [name, catalogVersion ?? version] as const;
+    });
+    dependencies = { ...dependencies, ...Object.fromEntries(resolvedDependencies) };
   }
   return dependencies;
 };
+
+type DependencyCatalog = Map<string, string>;
+const parseDependencyCatalogsAsync = async (): Promise<DependencyCatalog> => {
+  const currentDir = path.dirname(fileURLToPath(import.meta.url));
+  const workspaceConfigContent = await fsPromises.readFile(
+    path.join(currentDir, "..", "..", "..", "..", "pnpm-workspace.yaml"),
+    "utf-8",
+  );
+  const workspaceConfig: unknown = parseYaml(workspaceConfigContent);
+  const parseResult = workspaceConfigCatalogSchema.parse(workspaceConfig);
+
+  return new Map(Object.entries(parseResult.catalog ?? {}));
+};
+
+const workspaceConfigCatalogSchema = z.object({
+  catalog: z.record(z.string(), z.string()).optional(),
+});
 
 export type PackageJsonDependencies = Record<string, string>;
 interface PackageJson {
