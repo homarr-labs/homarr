07cb5a75617bfc5216ad76780fea9f16fde3c631
Thomas <thomas@ajnart.dev>
2026-05-06 23:34:39 +0200

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -41,7 +41,7 @@ else
     REDIS_PID=$!
 fi
 
-NODE_OPTIONS="--max-old-space-size=${NEXTJS_HEAP_SIZE:-256}" node apps/nextjs/server.js &
+node apps/nextjs/server.js &
 NEXTJS_PID=$!
 
 # Function to handle SIGTERM and shut down services
