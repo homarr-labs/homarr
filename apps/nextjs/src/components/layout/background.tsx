9513ea649604be86f4e32e28d4486ecc795fc86d
Meier Lukas <meierschlumpf@gmail.com>
2026-05-25 11:12:48 +0200

    fix: lint issue

--- apps/nextjs/src/components/layout/background.tsx
+++ apps/nextjs/src/components/layout/background.tsx
@@ -4,7 +4,10 @@ import type { AppShellProps } from "@mantine/core";
 import { useOptionalBoard } from "@homarr/boards/context";
 
 const supportedVideoFormats = ["mp4", "webm", "ogg"];
-const isVideo = (url: string) => supportedVideoFormats.some((format) => url.toLowerCase().endsWith(`.${format}`));
+const isVideo = (url: string) =>
+  supportedVideoFormats.some((format) =>
+    url.toLowerCase().endsWith(`.${format}`),
+  );
 
 export const useOptionalBackgroundProps = (): Partial<AppShellProps> => {
   const board = useOptionalBoard();
@@ -44,6 +47,7 @@ export const BoardBackgroundVideo = () => {
       muted
       loop
       playsInline
+      aria-hidden="true"
       style={{
         position: "fixed",
         width: "100vw",
