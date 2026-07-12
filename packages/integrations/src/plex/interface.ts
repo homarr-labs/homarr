64cc77bf72f9245550b7a60b9fbc7a38455ba18d
Bergasha <bergasha@hotmail.com>
2026-07-13 07:40:35 +1000

    Add interfaces for Stream, Part, Media, and TranscodeSession

--- packages/integrations/src/plex/interface.ts
+++ packages/integrations/src/plex/interface.ts
@@ -3,6 +3,54 @@ interface MediaContainer {
   Track?: Session[];
 }
 
+interface Stream {
+  $: {
+    streamType: string;
+    codec?: string;
+    language?: string;
+    selected?: string;
+    width?: string;
+    height?: string;
+    frameRate?: string;
+    channels?: string;
+  };
+}
+
+interface Part {
+  Stream?: Stream[];
+  $: {
+    container?: string;
+  };
+}
+
+interface Media {
+  Part?: Part[];
+  $: {
+    videoResolution?: string;
+    videoCodec?: string;
+    audioCodec?: string;
+    bitrate?: string;
+    container?: string;
+  };
+}
+
+interface TranscodeSession {
+  $: {
+    throttled?: string;
+    progress?: string;
+    speed?: string;
+    videoDecision?: string;
+    audioDecision?: string;
+    sourceVideoCodec?: string;
+    sourceAudioCodec?: string;
+    videoCodec?: string;
+    audioCodec?: string;
+    container?: string;
+    width?: string;
+    height?: string;
+  };
+}
+
 interface Session {
   User?: {
     $: {
@@ -15,13 +63,20 @@ interface Session {
     $: {
       product: string;
       title: string;
+      address?: string;
+      state?: string;
+      local?: string;
     };
   }[];
   Session?: {
     $: {
       id: string;
+      bandwidth?: string;
+      location?: string;
     };
   }[];
+  TranscodeSession?: TranscodeSession[];
+  Media?: Media[];
   $: {
     grandparentTitle?: string;
     parentTitle?: string;
@@ -29,6 +84,8 @@ interface Session {
     index?: number;
     type: string;
     live?: string;
+    viewOffset?: string;
+    duration?: string;
   };
 }
 
