e35603b9569374539d78f47a692b2935142eafb7
arturict <arturict@users.noreply.github.com>
2026-07-12 23:26:38 +0200

    Keep existing Nextcloud DAV endpoints intact

--- packages/integrations/src/nextcloud/nextcloud-url.ts
+++ packages/integrations/src/nextcloud/nextcloud-url.ts
@@ -1,8 +1,11 @@
 export const createNextcloudCalendarServerUrl = (integrationUrl: string) => {
   const calendarServerUrl = new URL(integrationUrl);
   const integrationPath = calendarServerUrl.pathname.replace(/\/+$/, "");
+  const davPath = "/remote.php/dav";
 
-  calendarServerUrl.pathname = `${integrationPath}/remote.php/dav/`;
+  calendarServerUrl.pathname = integrationPath.endsWith(davPath)
+    ? `${integrationPath}/`
+    : `${integrationPath}${davPath}/`;
   calendarServerUrl.search = "";
   calendarServerUrl.hash = "";
 
