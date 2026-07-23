eaaeaaccdb34d075c2f456daa0d4574e1ccd7875
ajnart <thomas@ajnart.fr>
2026-07-23 02:10:42 +0200

    fix: address board review and CI feedback

--- apps/nextjs/src/components/board/use-mobile-board.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { useMediaQuery } from "@mantine/hooks";
-
-export const mobileBoardMediaQuery = "(max-width: 48em)";
-
-export const useIsMobileBoard = () => useMediaQuery(mobileBoardMediaQuery);
