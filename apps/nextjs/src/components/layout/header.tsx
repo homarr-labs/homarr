d5dbf6fc17d50e925ebbca888875ae90e0aece0d
Thomas Camlong <thomas@ajnart.fr>
2026-08-03 16:46:43 +0200

    fix: contain board canvas overflow

--- apps/nextjs/src/components/layout/header.tsx
+++ apps/nextjs/src/components/layout/header.tsx
@@ -18,7 +18,7 @@ interface Props {
 
 export const MainHeader = ({ logo, actions, hasNavigation = true }: Props) => {
   return (
-    <AppShellHeader maw="100vw" zIndex={201} style={{ overflowX: "hidden" }}>
+    <AppShellHeader maw="100vw" zIndex={201} style={{ overflowX: "hidden" }} data-app-shell-header>
       <Group h="100%" gap="xl" px="md" justify="apart" wrap="nowrap">
         <Group h="100%" align="center" style={{ flex: 1 }} wrap="nowrap">
           {hasNavigation && <ClientBurger />}
