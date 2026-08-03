ee51227a5582b3bba3f82c12c0408c4b95bf2175
Thomas Camlong <thomas@ajnart.fr>
2026-08-03 18:53:26 +0200

    fix: restore notifications query scope

--- packages/widgets/src/notifications/index.ts
+++ packages/widgets/src/notifications/index.ts
@@ -2,12 +2,18 @@ import { IconMessage } from "@tabler/icons-react";
 
 import { getIntegrationKindsByCategory } from "@homarr/definitions";
 
-import { createWidgetDefinition } from "../definition";
+import { createWidgetDefinition, widgetQueryInputMatches } from "../definition";
 import { optionsBuilder } from "../options";
 
 export const { componentLoader, definition } = createWidgetDefinition("notifications", {
   supportsAdvancedFocus: false,
   icon: IconMessage,
+  queryKey: [["widget", "notifications", "getNotifications"]],
+  queryMatcher: ({ input }, scope) =>
+    widgetQueryInputMatches(input, {
+      hideLogos: scope.options.hideLogos,
+      integrationIds: scope.integrationIds,
+    }),
   createOptions() {
     return optionsBuilder.from((factory) => ({
       hideLogos: factory.switch({ defaultValue: false }),
