38fc9bfd0136d9eb506db5a8bad29845ac542521
Thomas Camlong <thomas@ajnart.fr>
2026-08-08 09:09:19 +0200

    chore: remove full-screen loading spinners from boards and manage

--- apps/nextjs/src/app/[locale]/boards/(content)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Loader } from "@mantine/core";
-
-export default function BoardLoading() {
-  return (
-    <Center w="100%" h="100vh">
-      <Loader size="xl" />
-    </Center>
-  );
-}
--- apps/nextjs/src/app/[locale]/manage/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Loader } from "@mantine/core";
-
-export default function ManageLoading() {
-  return (
-    <Center w="100%" h="100vh">
-      <Loader size="lg" />
-    </Center>
-  );
-}
