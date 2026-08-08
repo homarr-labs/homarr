87b2c0cdcc7c108fa6cbdd15944070f27903528e
Thomas Camlong <thomas@ajnart.fr>
2026-08-08 23:24:13 +0200

    fix(assistant): preserve lazy board boundaries

--- packages/spotlight/src/index.ts
+++ packages/spotlight/src/index.ts
@@ -1,13 +1,8 @@
 "use client";
 
-import { spotlightActions } from "./spotlight-store";
-
-export { Spotlight } from "./components/spotlight";
 export { openSpotlight, openMediaRequestSearch, mediaRequestSearchEvent } from "./open";
 export {
   SpotlightProvider,
   useRegisterSpotlightContextResults,
   useRegisterSpotlightContextActions,
 } from "./modes/home/context";
-
-export const closeSpotlight = spotlightActions.close;
