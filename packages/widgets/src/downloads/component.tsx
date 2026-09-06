b54a25b2c84e28ebbed37605b50edc69ebe58eb8
ajnart <thomas@ajnart.dev>
2026-09-06 11:01:02 +0200

    fix(downloads): preserve name column width

--- packages/widgets/src/downloads/component.tsx
+++ packages/widgets/src/downloads/component.tsx
@@ -493,6 +493,7 @@ export default function DownloadClientsWidget({
         title: t("items.name.columnTitle"),
         sortable: true,
         ellipsis: true,
+        width: 240,
         render: (record) => (
           <Tooltip
             key={displayMode}
